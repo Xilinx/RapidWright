b0830b6f222215c6b037b34c176c97dc87e4650c
Keith Rothman <537074+litghost@users.noreply.github.com>
2020-09-22 15:06:31 -0700

    Add helper script to invoke RapidWright entry points.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

---
+++ scripts/invoke_rapidwright.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -e
+# This script is a simple wrapper for invoking RapidWright entry points.
+#
+# Usage:
+#
+#    invoke_rapidwright.sh <class entry point> <arguments to entry point ...>
+#
+#  This script requires that RAPIDWRIGHT_PATH environment variable is defined.
+#
+#  RAPIDWRIGHT_PATH should point to the installation folder for RapidWright.
+#
+#  A Java runtime environment is required.  By default script will invoke
+#  the executable "java" if it is on the path.  If "java" is not on the path,
+#  the environment variable JAVA is used.  If "java" is not on the path and
+#  the environment variable JAVA is not defined or does not point to a valid
+#  executable, an error is generated.
+
+if [ "$#" -eq 0 ]; then
+    echo "Usage: invoke_rapidwright.sh <class entry point> <arguments to entry point ...>"
+    exit 1
+fi
+
+# Check if RAPIDWRIGHT_PATH exists and points to a valid directory.
+if [ -z ${RAPIDWRIGHT_PATH+x} ]; then
+    echo "RAPIDWRIGHT_PATH environment variable is not set."
+    exit 1
+fi
+
+if [ ! -d "${RAPIDWRIGHT_PATH}" ]; then
+    echo "RAPIDWRIGHT_PATH ('${RAPIDWRIGHT_PATH}') is not a valid directory?"
+    exit 1
+fi
+
+# Make sure rapidwright_path.sh is found at expected location.
+if [ ! -f "${RAPIDWRIGHT_PATH}/bin/rapidwright_classpath.sh" ]; then
+    echo "${RAPIDWRIGHT_PATH}/bin/rapidwright_classpath.sh was not found, check if RapidWright has been built."
+    exit 1
+fi
+
+# Find JRE executable, defaulting to executable java if JAVA environment
+# variable is not set.
+JAVA="${JAVA:-$(which java)}"
+
+if [ "${JAVA}" == "" ]; then
+    echo "Environment variable JAVA not defined, and java is not on path?"
+    exit 1
+fi
+
+if [[ ! -x "${JAVA}" ]]; then
+    echo "JAVA ('${JAVA}') is not executable?"
+    exit 1
+fi
+
+# Set the CLASSPATH to include RapidWright jars.
+source "${RAPIDWRIGHT_PATH}/bin/rapidwright_classpath.sh"
+
+# Invoke JRE with specified entry point and arguments.  Return the exit code
+# from java to caller of script.
+set +e
+"${JAVA}" "$@"
+exit $?
