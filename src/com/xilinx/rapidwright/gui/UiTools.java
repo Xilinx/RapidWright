8e13b9d3c1b038f7d73b6467fd27b8547414b89b
Jakob Wenzel <jakobw@xilinx.com>
2021-04-14 11:18:52 +0200

    add saving to svg
    Signed-off-by: Jakob Wenzel <jakobw@xilinx.com>

--- src/com/xilinx/rapidwright/gui/UiTools.java
+++ src/com/xilinx/rapidwright/gui/UiTools.java
@@ -4,10 +4,26 @@ import java.io.File;
 
 import com.trolltech.qt.gui.QPainter;
 import com.trolltech.qt.gui.QPrinter;
+import com.trolltech.qt.svg.QSvgGenerator;
 
 public class UiTools {
     private UiTools(){
 
+    }
+
+    public static void saveAsSvg(TileScene scene, File file) {
+
+        QSvgGenerator svgGen = new QSvgGenerator();
+
+        svgGen.setFileName(file.toString());
+        svgGen.setSize(scene.getSceneSize());
+        svgGen.setViewBox(scene.sceneRect());
+        svgGen.setTitle("RapidWright Scene");
+        svgGen.setDescription(scene.getClass().getSimpleName());
+
+        QPainter svgPainter = new QPainter(svgGen);
+        scene.render(svgPainter);
+        svgPainter.end();
     }
 
     public static void saveAsPdf(TileScene scene, File file) {
