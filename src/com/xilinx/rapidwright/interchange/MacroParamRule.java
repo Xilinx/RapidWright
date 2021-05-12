a9f6c4303bcb4b76040d34119716164b5a82b348
gatecat <gatecat@ds0.me>
2021-05-12 13:20:47 +0100

    interchange: Add example parameter rules for a couple of macros
    Signed-off-by: gatecat <gatecat@ds0.me>

--- src/com/xilinx/rapidwright/interchange/MacroParamRule.java
+++ src/com/xilinx/rapidwright/interchange/MacroParamRule.java
@@ -1,5 +1,7 @@
 package com.xilinx.rapidwright.interchange;
 
+import com.xilinx.rapidwright.design.Unisim;
+
 public class MacroParamRule {
     public String primParam;
     public String instName;
@@ -19,10 +21,10 @@ public class MacroParamRule {
         return new MacroParamRule(primParam, instName, instParam);
     }
 
-    public static MacroParamRule bit_range(String primParam, String instName, String instParam, int start, int end) {
+    public static MacroParamRule bit_range(String primParam, String instName, String instParam, int start, int count) {
         MacroParamRule result = new MacroParamRule(primParam, instName, instParam);
-        result.bitSlice = new int[(end-start) + 1];
-        for (int i = 0; i <= (end-start); i++)
+        result.bitSlice = new int[count];
+        for (int i = 0; i < count; i++)
             result.bitSlice[i] = start + i;
         return result;
     }
@@ -31,5 +33,36 @@ public class MacroParamRule {
         MacroParamRule result = new MacroParamRule(primParam, instName, instParam);
         result.tableLookup = table;
         return result;
+    }
+
+    public static MacroParamRule[] getRules(Unisim prim) {
+        switch (prim) {
+            case LUT6_2:
+                return new MacroParamRule[] {
+                    bit_range("INIT", "LUT5", "INIT", 0, 32),
+                    bit_range("INIT", "LUT6", "INIT", 0, 64),
+                };
+            case RAM64X1D:
+                return new MacroParamRule[] {
+                    bit_range("INIT", "DP", "INIT", 0, 64),
+                    bit_range("INIT", "SP", "INIT", 0, 64),
+                };
+            case RAM128X1D:
+                return new MacroParamRule[] {
+                    bit_range("INIT", "DP.LOW", "INIT", 0,  64),
+                    bit_range("INIT", "DP.HIGH", "INIT", 64, 64),
+                    bit_range("INIT", "SP.LOW", "INIT", 0,  64),
+                    bit_range("INIT", "SP.HIGH", "INIT", 64, 64),
+                };
+            case RAM64M:
+                return new MacroParamRule[] {
+                    bit_range("INIT_A", "RAMA", "INIT", 0, 64),
+                    bit_range("INIT_B", "RAMB", "INIT", 0, 64),
+                    bit_range("INIT_C", "RAMC", "INIT", 0, 64),
+                    bit_range("INIT_D", "RAMD", "INIT", 0, 64),
+                };
+            default:
+                return new MacroParamRule[0];
+        }
     }
 }
