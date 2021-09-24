6001ce2dd8c4fbe90a831503b4c8ea1cb22081c3
Jakob Wenzel <wenzel@rs.tu-darmstadt.de>
2021-09-24 11:49:26 +0200

    add classes to dump site insts, edif or physical netlists as dot graphs
    Signed-off-by: Jakob Wenzel <wenzel@rs.tu-darmstadt.de>

---
+++ src/com/xilinx/rapidwright/debug/DotPhysicalDumper.java
@@ -0,0 +1,134 @@
+package com.xilinx.rapidwright.debug;
+
+import com.xilinx.rapidwright.design.Design;
+import com.xilinx.rapidwright.design.Net;
+import com.xilinx.rapidwright.design.SiteInst;
+import com.xilinx.rapidwright.design.SitePinInst;
+
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Dump a Design's physical representation to a Graphviz Dot Graph
+ */
+public class DotPhysicalDumper extends DotGraphDumper<SiteInst, SitePinInst, Void, Net, Design> {
+
+    public DotPhysicalDumper() {
+        super(true);
+    }
+
+    public DotPhysicalDumper(boolean makeNetNode) {
+        super(makeNetNode);
+    }
+
+    @Override
+    protected Stream<SiteInst> getInstances(Design design) {
+        return design.getSiteInsts().stream();
+    }
+
+    @Override
+    protected Stream<SitePinInst> getPorts(SiteInst siteInst) {
+        return siteInst.getSitePinInsts().stream();
+    }
+
+    @Override
+    protected Stream<Void> getPortTemplates(SiteInst siteInst) {
+        return Stream.empty();
+    }
+
+    @Override
+    protected Stream<Net> getNets(Design design) {
+        return design.getNets().stream().filter(n->n.getPins().size()>0);
+    }
+
+    @Override
+    protected Stream<SitePinInst> getNetPorts(Net net) {
+        return net.getPins().stream();
+    }
+
+    @Override
+    protected boolean isOutputPort(SitePinInst sitePinInst) {
+        return sitePinInst.isOutPin();
+    }
+
+    @Override
+    protected boolean isOutputPortTemplate(Void port) {
+        throw new IllegalStateException("Does not have templates");
+    }
+
+    @Override
+    protected String getInstanceName(SiteInst si) {
+        return si.getName();
+    }
+
+    @Override
+    protected String getPortName(SitePinInst sitePinInst) {
+        return sitePinInst.getName();
+    }
+
+    @Override
+    protected String getPortTemplateName(Void port) {
+        throw new IllegalStateException("Does not have templates");
+    }
+
+    @Override
+    protected Stream<SitePinInst> getRootPorts(Design design) {
+        return Stream.empty();
+    }
+
+    @Override
+    protected String getNetName(Net net) {
+        return net.getName();
+    }
+
+    @Override
+    protected Map<?, ?> getInstanceProperties(SiteInst siteInst, Design design) {
+        return siteInst.getCells().stream().distinct().collect(Collectors.toMap(Function.identity(), x->""));
+    }
+
+
+
+    /**
+     * Dump a physical netlist to a file while filtering the SiteInsts that are shown
+     * @param to the target file
+     * @param design the design to dump
+     * @param filter A function that filters the instances that are shown
+     */
+    public static void dump(Path to, Design design, BiPredicate<SiteInst, Design> filter) {
+        new DotPhysicalDumper().doDump(design, to, filter);
+    }
+
+    /**
+     * Dump a physical netlist to a file while filtering the SiteInsts that are shown
+     * @param to the target file
+     * @param design the design to dump
+     * @param filter A function that filters the instances that are shown
+     */
+    public static void dump(Path to, Design design,Predicate<SiteInst> filter) {
+        dump(to, design, (i, d)->filter.test(i));
+    }
+
+    /**
+     * Dump a physical netlist to a file
+     * @param to the target file
+     * @param design the design to dump
+     */
+    public static void dump(Path to, Design design) {
+        dump(to, design, (BiPredicate<SiteInst, Design>) null);
+    }
+
+    @Override
+    protected SiteInst getPortInstance(SitePinInst port) {
+        return port.getSiteInst();
+    }
+
+    @Override
+    protected Net getPortNet(SitePinInst p) {
+        return p.getNet();
+    }
+}
