ae4ffe44d6ffeeeca0e1a59df19c5b73ddedb96e
Chris Lavin <clavin@xilinx.com>
2021-09-30 13:25:45 -0600

    Auto populate versions, removes internal classpath/jvm args
    Signed-off-by: Chris Lavin <clavin@xilinx.com>

--- python/src/rapidwright/rapidwright.py
+++ python/src/rapidwright/rapidwright.py
@@ -24,22 +24,20 @@ from jpype.types import *
 from typing import List, Optional
 import os, urllib.request, platform
 
-def start_jvm(add_classpath=None, jvm_flags=[], rw_version="2021.1.0"):
+version='2021.1.1'
+
+def start_jvm():
     os_str = 'lin64'
     if platform.system() == 'Windows':
         os_str = 'win64'
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    file_name = "rapidwright-"+rw_version+"-standalone-"+os_str+".jar"
+    file_name = "rapidwright-"+version+"-standalone-"+os_str+".jar"
     classpath = os.path.join(dir_path,file_name)
-    print("file=" + classpath)
     if not os.path.isfile(classpath):
-        url = "http://github.com/Xilinx/RapidWright/releases/download/v"+rw_version+"-beta/" + file_name
+        url = "http://github.com/Xilinx/RapidWright/releases/download/v"+version+"-beta/" + file_name
         urllib.request.urlretrieve(url,classpath)
-
-    if add_classpath != None:
-        classpath = classpath.insert(0, [add_classpath])
         
     if not jpype.isJVMStarted():
-        jpype.startJVM(classpath=classpath, *jvm_flags)
+        jpype.startJVM(classpath=classpath)
 
 start_jvm()
