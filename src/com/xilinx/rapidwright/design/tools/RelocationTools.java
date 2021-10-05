c556479c48406a86489c888276ff119918261a92
eddieh-xlnx <90657806+eddieh-xlnx@users.noreply.github.com>
2021-10-05 09:41:16 -0700

    Add rapidwright.design.tools.RelocationTools (#245)
    * Rename to TestRelocateTile.java
    
    * First stab at cell relocation
    
    * Refactor and rename
    
    * Rename to RelocationTools
    
    * Address review, more robustness, add doc
    
    * HashMap.Entry -> Map.Entry
    
    * Add RapidWrightDCP submodule
    
    * Update messaging, skip if row/col == 0
    
    * Add tests
    
    * Checkout submodules too
    
    * Add --debug
    
    * Update submodule
    
    * Revert "Add --debug"
    
    This reverts commit 20e1c74d8c3896fbac67eda632783301a610d1d0.
    
    * Add @CheckOpenFiles
    
    * Unplace all before placing and checking if occupied
    
    * Add module moving tests
    
    * Update DCP submodule
    
    * Finish comment
    
    * Whitespace
    
    * Use design.isSiteUsed() for clarity
    
    * Update for fixed DCPs
    
    * Use net.unroutePin
    
    * Infer physical Cell-s from logical Cell hierarchy
    
    * Update doc
    
    * Refactor, remove workaround
    
    * Add relocate by pblock with tests
    
    * Remove extra newlines
    
    * Many improvements, add ability to relocate PBlocks
    
    * Add failing MicroBlazeAndILA test
    
    * Use new Utils.sliceDspBramUramTypes for whitelist
    
    * Remove CFGLUT5 workaround
    
    * Resolve review comments
    
    Co-authored-by: eddieh-xlnx <eddieh-xlnx@users.noreply.github.com>

---
+++ src/com/xilinx/rapidwright/design/tools/RelocationTools.java
@@ -0,0 +1,299 @@
+/*
+ * Copyright (c) 2021 Xilinx, Inc.
+ * All rights reserved.
+ *
+ * Author: Eddie Hung, Xilinx Research Labs.
+ *
+ * This file is part of RapidWright.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.xilinx.rapidwright.design.tools;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.xilinx.rapidwright.design.Cell;
+import com.xilinx.rapidwright.design.Design;
+import com.xilinx.rapidwright.design.DesignTools;
+import com.xilinx.rapidwright.design.Net;
+import com.xilinx.rapidwright.design.SiteInst;
+import com.xilinx.rapidwright.design.SitePinInst;
+import com.xilinx.rapidwright.design.blocks.PBlock;
+import com.xilinx.rapidwright.device.PIP;
+import com.xilinx.rapidwright.device.Site;
+import com.xilinx.rapidwright.device.Tile;
+import com.xilinx.rapidwright.edif.EDIFHierCellInst;
+import com.xilinx.rapidwright.edif.EDIFNetlist;
+import com.xilinx.rapidwright.util.Pair;
+import com.xilinx.rapidwright.util.Utils;
+
+/**
+ * A collection of tools to help relocate designs.
+ *
+ * @author eddieh
+ *
+ */
+public class RelocationTools {
+
+    /**
+     * Relocate all SiteInsts (and all associated PIPs) belonging to the logical Cell at
+     * instanceName in-place by tileColOffset/tileRowOffset tiles.
+     *
+     * Should a SiteInst contain a mix of physical Cells belonging to and outside of
+     * instanceName then this function will fail.
+     *
+     * @param design Parent design
+     * @param instanceName Full hierarchical instance name to logical cell
+     *                     (empty for top cell)
+     * @param tileColOffset Relocate this number of tile columns (X axis)
+     * @param tileRowOffset Relocate this number of tile rows (Y axis)
+     * @return True if successful, false otherwise.
+     */
+    public static boolean relocate(Design design,
+                                   String instanceName,
+                                   int tileColOffset,
+                                   int tileRowOffset) {
+        EDIFNetlist netlist = design.getNetlist();
+        EDIFHierCellInst instanceCell = netlist.getHierCellInstFromName(instanceName);
+        if (instanceCell == null) {
+            System.out.println("ERROR: Logical cell with instance name '" + instanceName + "' not found");
+            return false;
+        }
+
+        Set<Cell> cells = new HashSet<>();
+        Set<SiteInst> siteInsts = new HashSet<>();
+        for (EDIFHierCellInst leaf : netlist.getAllLeafDescendants(instanceCell)) {
+            String leafName = leaf.getCellName();
+            if (leafName.equals("GND") || leafName.equals("VCC")) {
+                continue;
+            }
+
+            Cell c = design.getCell(leaf.getFullHierarchicalInstName());
+            if (c == null) {
+                System.out.println("WARNING: Could not find physical cell corresponding to logical cell '" +
+                        leaf.getFullHierarchicalInstName() + "'; ignoring");
+                continue;
+            }
+            cells.add(c);
+
+            SiteInst si = c.getSiteInst();
+            if (si == null) {
+                continue;
+            }
+
+            if (!Utils.sliceDspBramUramTypes.contains(si.getSiteTypeEnum())) {
+                System.out.println("WARNING: Skipping cell '" + leaf.getFullHierarchicalInstName() +
+                        "' as it is placed onto a SiteInst type '" + si.getSiteTypeEnum() + "'");
+                continue;
+            }
+
+            siteInsts.add(si);
+        }
+
+        // Check that every SiteInst only contains Cells part of the hierarchy
+        boolean error = false;
+        for (SiteInst si : siteInsts) {
+            for (Cell c : si.getCells()) {
+                if (!c.isLocked() && !cells.contains(c)) {
+                    System.out.println("ERROR: Failed to relocate SiteInst '" + si.getName()
+                            + "' as it contains Cells both inside and outside of '" + instanceName + "'");
+                    error = true;
+                }
+            }
+        }
+
+        return !error && relocate(design, siteInsts, tileColOffset, tileRowOffset);
+    }
+
+    /**
+     * Relocate all SiteInsts (and PIPs) within the Pblock in-place by tileColOffset/tileRowOffset tiles.
+     *
+     * @param design Parent design
+     * @param pblock PBlock
+     * @param tileColOffset Relocate this number of tile columns (X axis)
+     * @param tileRowOffset Relocate this number of tile rows (Y axis)
+     * @return True if successful, false otherwise.
+     */
+    public static boolean relocate(Design design,
+                                   PBlock pblock,
+                                   int tileColOffset,
+                                   int tileRowOffset) {
+        Collection<SiteInst> siteInsts = new ArrayList<>();
+        for (Site s : pblock.getAllSites(null)) {
+            SiteInst si = design.getSiteInstFromSite(s);
+            if (si != null) {
+                siteInsts.add(si);
+            }
+        }
+        return relocate(design, siteInsts, tileColOffset, tileRowOffset);
+    }
+
+    /**
+     * Relocate all given SiteInsts and PIPs in-place by
+     * tileColOffset/tileRowOffset tiles.
+     *
+     * Should any SiteInst or PIP not be relocatable to a tile at the specified
+     * offset (e.g. if a compatible tile does not exist) function will return
+     * false and design will be unmodified.
+     *
+     * Any net sourced from a SiteInst not in the given set will be fully
+     * unrouted; those destined for a SiteInst not in the given set will have
+     * their specific branch unrouted.
+     *
+     * @param design Parent design
+     * @param siteInsts List of SiteInsts to be relocated
+     * @param tileColOffset Relocate this number of tile columns (X axis)
+     * @param tileRowOffset Relocate this number of tile rows (Y axis)
+     * @return True if successful, false otherwise.
+     */
+    public static boolean relocate(Design design,
+                                   Collection<SiteInst> siteInsts,
+                                   int tileColOffset,
+                                   int tileRowOffset) {
+        if (siteInsts.isEmpty())
+            return true;
+
+        if (tileColOffset == 0 && tileRowOffset == 0)
+            return true;
+
+        Map<SiteInst, Site> oldSite = new HashMap<>();
+        for (SiteInst si : siteInsts) {
+            assert(si.isPlaced());
+            oldSite.put(si, si.getSite());
+            si.unPlace();
+        }
+
+        boolean revertPlacement = false;
+        for (Map.Entry<SiteInst, Site> e : oldSite.entrySet()) {
+            Site srcSite = e.getValue();
+            Tile srcTile = srcSite.getTile();
+            Tile destTile = srcTile.getTileXYNeighbor(tileColOffset, tileRowOffset);
+            Site destSite = srcSite.getCorrespondingSite(srcSite.getSiteTypeEnum(), destTile);
+            SiteInst srcSiteInst = e.getKey();
+            assert(destSite != srcSite);
+            if (destTile == null || destSite == null) {
+                String destTileName = srcTile.getNameRoot() + "_X" + (srcTile.getTileXCoordinate() + tileColOffset)
+                        + "Y" + (srcTile.getTileYCoordinate() + tileRowOffset);
+                System.out.println("ERROR: Failed to move SiteInst '" + srcSiteInst.getName() + "' from Tile '" + srcTile.getName()
+                        + "' to Tile '" + destTileName + "'");
+                revertPlacement = true;
+                continue;
+            }
+            SiteInst destSiteInst = design.getSiteInstFromSite(destSite);
+            if (destSiteInst != null) {
+                if (destSiteInst.getName().startsWith("STATIC_SOURCE")) {
+                    destSiteInst.unPlace();
+                } else {
+                    System.out.println("ERROR: Failed to move SiteInst '" + srcSiteInst.getName() + "' from Tile '" + srcTile.getName()
+                            + "' to Tile '" + destTile.getName() + "' as it is already occupied");
+                    revertPlacement = true;
+                    continue;
+                }
+            }
+
+            srcSiteInst.place(destSite);
+        }
+
+        if (revertPlacement) {
+            revertPlacement(oldSite);
+            return false;
+        }
+
+        List<Pair<Net, List<PIP>>> oldRoute = new ArrayList<>();
+        boolean revertRouting = false;
+
+        DesignTools.createMissingSitePinInsts(design);
+
+        for (Net n : design.getNets()) {
+            if (!n.hasPIPs()) {
+                continue;
+            }
+
+            SitePinInst src = n.getSource();
+            if (src != null && !oldSite.containsKey(src.getSiteInst())) {
+                System.out.println("INFO: Unrouting Net '" + n.getName() + "' since output SiteInstPin '" +
+                        src + "' does not belong to SiteInsts to be relocated");
+                n.unroute();
+                continue;
+            }
+
+            Collection<SitePinInst> pins = n.getPins();
+            Collection<SitePinInst> nonMatchingPins = pins.stream().filter(
+                    (spi) -> !oldSite.containsKey(spi.getSiteInst())).collect(Collectors.toList());
+            if (nonMatchingPins.size() == pins.size()) {
+                continue;
+            }
+
+            oldRoute.add(new Pair<>(n, n.getPIPs()));
+
+            if (!nonMatchingPins.isEmpty()) {
+                for (SitePinInst spi : nonMatchingPins) {
+                    System.out.println("INFO: Unrouting SitePinInst '" + spi + "' branch of Net '" + n.getName() +
+                            "' since it does not belong to SiteInsts to be relocated");
+                    n.unroutePin(spi);
+                }
+            }
+
+            boolean isClockNet = n.isClockNet() || n.hasGapRouting();
+            for (PIP sp : n.getPIPs()) {
+                Tile st = sp.getTile();
+                Tile dt = st.getTileXYNeighbor(tileColOffset, tileRowOffset);
+                if (dt == null) {
+                    if (isClockNet) {
+                        System.out.println("INFO: Skipping clock net PIP '" + sp + "' (Net '" + n.getName() + "')");
+                    } else {
+                        String destTileName = st.getNameRoot() + "_X" + (st.getTileXCoordinate() + tileColOffset)
+                                + "Y" + (st.getTileYCoordinate() + tileRowOffset);
+                        System.out.println("ERROR: Failed to move PIP '" + sp + "' to Tile '" + destTileName +
+                                "' (Net '" + n.getName() + "')");
+                        revertRouting = true;
+                        throw new RuntimeException();
+                    }
+                } else {
+                    assert (st.getTileTypeEnum() == dt.getTileTypeEnum());
+                    sp.setTile(dt);
+                }
+            }
+        }
+
+        if (revertRouting) {
+            revertPlacement(oldSite);
+            revertRouting(oldRoute);
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void revertRouting(List<Pair<Net, List<PIP>>> oldRoute) {
+        for (Pair<Net,List<PIP>> e : oldRoute) {
+            e.getFirst().setPIPs(e.getSecond());
+        }
+    }
+
+    private static void revertPlacement(Map<SiteInst, Site> oldSite) {
+        for (Map.Entry<SiteInst, Site> e : oldSite.entrySet()) {
+            e.getKey().unPlace();
+            e.getKey().place(e.getValue());
+        }
+    }
+}
---
+++ test/com/xilinx/rapidwright/design/TestRelocationTools.java
@@ -0,0 +1,243 @@
+/*
+ * Copyright (c) 2021 Xilinx, Inc.
+ * All rights reserved.
+ *
+ * Author: Eddie Hung, Xilinx Research Labs.
+ *
+ * This file is part of RapidWright.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.xilinx.rapidwright.design;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+import com.xilinx.rapidwright.checker.CheckOpenFiles;
+import com.xilinx.rapidwright.design.blocks.PBlock;
+import com.xilinx.rapidwright.design.tools.RelocationTools;
+import com.xilinx.rapidwright.device.Device;
+import com.xilinx.rapidwright.device.PIP;
+import com.xilinx.rapidwright.device.Site;
+import com.xilinx.rapidwright.device.SiteTypeEnum;
+import com.xilinx.rapidwright.device.Tile;
+import com.xilinx.rapidwright.util.Pair;
+import com.xilinx.rapidwright.tests.CodePerfTracker;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class TestRelocationTools {
+
+    private void relocateModuleInstsAndCompare(int colOffset, int rowOffset, boolean expectSuccess, Design design1, Design design2, Collection<ModuleInst> moduleInsts) {
+        List<Pair<ModuleInst,Site>> newSite = new ArrayList<>();
+        for (ModuleInst mi : moduleInsts) {
+            Assertions.assertTrue(mi.isPlaced());
+
+            SiteInst si = mi.getAnchor();
+            Site ss = si.getSite();
+            Tile st = si.getTile();
+            Tile dt = st.getTileXYNeighbor(colOffset, rowOffset);
+            Site ds = ss.getCorrespondingSite(si.getSiteTypeEnum(), dt);
+            newSite.add(new Pair<>(mi, ds));
+
+            if (!mi.getModule().isValidPlacement(ds, null /* Do not check existing placements */)) {
+                Assertions.assertFalse(expectSuccess);
+            }
+        }
+
+        for (Pair<ModuleInst,Site> e : newSite) {
+            ModuleInst mi = e.getFirst();
+            Site ds = e.getSecond();
+
+            if (expectSuccess) {
+                Assertions.assertEquals(mi.place(ds), expectSuccess);
+
+                for (SiteInst si : mi.getSiteInsts()) {
+                    for (Cell c2 : si.getCells()) {
+                        Cell c1 = design1.getCell(c2.getName());
+                        if (c1 == null && c2.getName().startsWith(mi.getName() + "/")) {
+                            // Retry without ModuleInst hierarchy in case it was flattened
+                            c1 = design1.getCell(c2.getName().substring(mi.getName().length() + 1));
+                        }
+                        Assertions.assertNotNull(c1);
+                        Assertions.assertEquals(c1.getSite(), c2.getSite());
+                    }
+                }
+
+                for (Net n2 : mi.getNets()) {
+                    Net n1 = design1.getNet(n2.getName());
+                    if (n1 == null && n2.getName().startsWith(mi.getName() + "/")) {
+                        // Retry without ModuleInst hierarchy in case it was flattened
+                        n1 = design1.getNet(n2.getName().substring(mi.getName().length() + 1));
+                    }
+                    if (n1 == null && (n2.isStaticNet() || n2.getName() == Net.USED_NET)) {
+                        // Module relocation does not propagate static nets nor USED_NETs
+                        continue;
+                    }
+                    Assertions.assertNotNull(n1);
+
+                    if (n1.isClockNet() || n1.hasGapRouting()) {
+                        // Module relocation unroutes all clock nets?
+                        Assertions.assertFalse(n2.hasPIPs());
+                        continue;
+                    }
+
+                    Set<PIP> p1 = new HashSet<>(n1.getPIPs());
+                    Set<PIP> p2 = new HashSet<>(n2.getPIPs());
+                    Assertions.assertEquals(p1, p2);
+                }
+            }
+        }
+    }
+
+    @ParameterizedTest(name = "Relocate PicoBlaze OOC '{0}' ({1},{2})")
+    @MethodSource()
+    @CheckOpenFiles
+    public void testPicoblazeOOC(String instanceName, int colOffset, int rowOffset, boolean expectSuccess) {
+        String dcpPath = "RapidWrightDCP/picoblaze_ooc_X10Y235.dcp";
+        Design design1 = Design.readCheckpoint(dcpPath, CodePerfTracker.SILENT);
+
+        Assertions.assertEquals(RelocationTools.relocate(design1, instanceName, colOffset, rowOffset),
+                expectSuccess);
+
+        String metaPath = "RapidWrightDCP/picoblaze_ooc_X10Y235.metadata";
+        if (instanceName.isEmpty()) {
+            Design design2 = new Design("design2", design1.getPartName());
+            Module module = new Module(Design.readCheckpoint(dcpPath, CodePerfTracker.SILENT), metaPath);
+            ModuleInst mi = design2.createModuleInst("inst", module);
+            mi.placeOnOriginalAnchor();
+            Collection<ModuleInst> moduleInsts = Arrays.asList(mi);
+
+            relocateModuleInstsAndCompare(colOffset, rowOffset, expectSuccess, design1, design2, moduleInsts);
+        }
+    }
+
+    public static Stream<Arguments> testPicoblazeOOC() {
+        return Stream.of(
+                Arguments.of("", 0, 5, true)
+                , Arguments.of("", 0, -5, true)
+                , Arguments.of("", 9, -5, true)
+                , Arguments.of("", 0, 0, true)
+                , Arguments.of("", 1, 0, false)     // Incompatible tile
+                , Arguments.of("", 0, 1, false)     // Incompatible tile
+                , Arguments.of("", -100, 0, false)  // Out of X range
+                , Arguments.of("", 0, 200, false)   // Out of Y range
+                , Arguments.of("processor", 9, 0, false) // SiteInsts contains matching and non-matching cells
+        );
+    }
+
+
+    private static final String Picoblaze4OOCdcp = "RapidWrightDCP/picoblaze4_ooc_X6Y60_X6Y65_X10Y60_X10Y65.dcp";
+
+    @ParameterizedTest(name = "Relocate PicoBlaze4 OOC '{0}' ({1},{2})")
+    @MethodSource()
+    @CheckOpenFiles
+    public void testPicoblaze4OOC(String instanceName, int colOffset, int rowOffset, boolean expectSuccess) {
+        Design design1 = Design.readCheckpoint(Picoblaze4OOCdcp, CodePerfTracker.SILENT);
+
+        Assertions.assertEquals(RelocationTools.relocate(design1, instanceName, colOffset, rowOffset),
+                expectSuccess);
+
+        Design design2 = Design.readCheckpoint(Picoblaze4OOCdcp, CodePerfTracker.SILENT);
+        Collection<ModuleInst> moduleInsts;
+        if (instanceName.isEmpty()) {
+            moduleInsts = design2.getModuleInsts();
+        } else {
+            moduleInsts = new ArrayList<>();
+            ModuleInst mi = design2.getModuleInst(instanceName);
+            Assertions.assertNotNull(mi);
+            moduleInsts = Arrays.asList(mi);
+        }
+
+        relocateModuleInstsAndCompare(colOffset, rowOffset, expectSuccess, design1, design2, moduleInsts);
+    }
+
+    public static Stream<Arguments> testPicoblaze4OOC() {
+        return Stream.of(
+                  Arguments.of("", 0, 5, true)
+                , Arguments.of("picoblaze_0_13", 0, 5, true)
+                , Arguments.of("picoblaze_0_12", 0, 5, false) // placement conflict
+        );
+    }
+
+    @ParameterizedTest(name = "Relocate PicoBlaze4 OOC PBlock '{0}' ({1},{2})")
+    @MethodSource()
+    @CheckOpenFiles
+    public void testPicoblaze4OOC_PBlock(PBlock pblock, int colOffset, int rowOffset, boolean expectSuccess) {
+        Design design1 = Design.readCheckpoint(Picoblaze4OOCdcp, CodePerfTracker.SILENT);
+
+        Assertions.assertEquals(RelocationTools.relocate(design1, pblock, colOffset, rowOffset),
+                expectSuccess);
+
+        Design design2 = Design.readCheckpoint(Picoblaze4OOCdcp, CodePerfTracker.SILENT);
+
+        // Find the set of ModuleInsts
+        Set<ModuleInst> moduleInsts1 = new HashSet<>();
+        for (Site s : pblock.getAllSites(null)) {
+            SiteInst si = design1.getSiteInstFromSite(s);
+            if (si != null) {
+                ModuleInst mi = si.getModuleInst();
+                if (mi != null) {
+                    moduleInsts1.add(mi);
+                }
+            }
+        }
+
+        Collection<ModuleInst> moduleInsts2 = moduleInsts1.stream().map((mi) -> design2.getModuleInst(mi.getName()))
+                .collect(Collectors.toList());
+        relocateModuleInstsAndCompare(colOffset, rowOffset, expectSuccess, design1, design2, moduleInsts2);
+    }
+
+    public static Stream<Arguments> testPicoblaze4OOC_PBlock() {
+        final String partName = Design.getPartNameFromDCP(Picoblaze4OOCdcp);
+        return Stream.of(
+                  Arguments.of(new PBlock(Device.getDevice(partName), "SLICE_X8Y65:SLICE_X11Y69 RAMB18_X0Y26:RAMB18_X0Y27 RAMB36_X0Y13:RAMB36_X0Y13"),
+                        0, 5, true)
+                , Arguments.of(new PBlock(Device.getDevice(partName), "SLICE_X8Y60:SLICE_X11Y64 RAMB18_X0Y24:RAMB18_X0Y25 RAMB36_X0Y12:RAMB36_X0Y12"),
+                        0, 5, false) // placement conflict
+        );
+    }
+
+    @ParameterizedTest(name = "Relocate MicroBlazeAndILA '{0}' ({1},{2})")
+    @MethodSource()
+    @CheckOpenFiles
+    public void testMicroBlazeAndILA(String instanceName, int colOffset, int rowOffset, boolean expectSuccess) {
+        String dcpPath = "RapidWrightDCP/microblazeAndILA_3pblocks.dcp";
+        Design design1 = Design.readCheckpoint(dcpPath, CodePerfTracker.SILENT);
+
+        Assertions.assertEquals(RelocationTools.relocate(design1, instanceName, colOffset, rowOffset),
+                expectSuccess);
+    }
+
+    public static Stream<Arguments> testMicroBlazeAndILA() {
+        return Stream.of(
+                  Arguments.of("", 0, 5, true)
+                , Arguments.of("", 0, 60, true)
+                , Arguments.of("base_mb_i", 0, 10, true)
+                , Arguments.of("dbg_hub", 0, 20, true)
+                , Arguments.of("u_ila_0", 0, 30, true)
+                , Arguments.of("dbg_hub", 16, 0, false) // placement conflict
+        );
+    }
+
+}
