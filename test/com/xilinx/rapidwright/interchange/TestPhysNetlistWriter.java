3850506e3dae83a37567e9cc31fb246d314a00cc
eddieh-xlnx <eddieh-xlnx@users.noreply.github.com>
2021-10-08 17:28:03 -0700

    Add header

--- test/com/xilinx/rapidwright/interchange/TestPhysNetlistWriter.java
+++ test/com/xilinx/rapidwright/interchange/TestPhysNetlistWriter.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2021 Xilinx, Inc.
+ * All rights reserved.
+ *
+ * Author: Eddie Hung, Xilinx Research Labs.
+ *
+ * This file is part of RapidWright.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.xilinx.rapidwright.interchange;
 
 import java.io.IOException;
@@ -25,10 +47,6 @@ import org.capnproto.MessageReader;
 import org.capnproto.ReaderOptions;
 import org.capnproto.StructList;
 
-/**
- * Test that we can write a DCP file and read it back in. We currently don't have a way to check designs for equality,
- * so we just try to catch obvious issues.
- */
 public class TestPhysNetlistWriter {
     private void testAllRouteSegmentsEndInBELInputPins(Design design, RouteBranch.Reader routeBranch, Enumerator<String> strings) {
         StructList.Reader<RouteBranch.Reader> branches = routeBranch.getBranches();
