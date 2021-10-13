d972db66d4d841137c883da181ddaaf13112296d
eddieh-xlnx <eddieh-xlnx@users.noreply.github.com>
2021-10-13 09:47:15 -0700

    Add first DRC -- LUT not routed through more than once

---
+++ src/com/xilinx/rapidwright/design/DRC.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2021 Xilinx, Inc.
+ * All rights reserved.
+ *
+ * Author: Eddie Hung, Xilinx Research Labs.
+ *
+ * This file is part of RapidWright.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.xilinx.rapidwright.design;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.xilinx.rapidwright.design.drc.DesignRuleCheckInterface;
+import com.xilinx.rapidwright.design.drc.NetRoutesThruLutAtMostOnce;
+
+public class DRC implements DesignRuleCheckInterface {
+    public static final List<Class<? extends DesignRuleCheckInterface>> checks =
+            new ArrayList<Class<? extends DesignRuleCheckInterface>>() {{
+        add(NetRoutesThruLutAtMostOnce.class);
+    }};
+
+    @Override
+    public int run(Design design) {
+        return checks.stream().map((c) -> {
+                    try {
+                        DesignRuleCheckInterface i = (DesignRuleCheckInterface)c.newInstance();
+                        return (int) c.getDeclaredMethod("run", Design.class).invoke(i, design);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .reduce(0, Integer::sum);
+    }
+
+    private static void printUsageAndExit() {
+        System.out.println("USAGE: <input.dcp> [--strict]");
+        System.exit(1);
+    }
+
+    public static void main(String[] args) {
+        if(args.length < 1 || args.length > 2) {
+            printUsageAndExit();
+        }
+
+        boolean strict = false;
+        if (args.length == 2) {
+            if (args[1].equals("--strict")) {
+                strict = true;
+            } else {
+                printUsageAndExit();
+            }
+        }
+
+        Design design = Design.readCheckpoint(args[0]);
+        int numFailed = new DRC().run(design);
+        if (numFailed > 0) {
+            String message = numFailed + " failed DRCs";
+            if (strict) {
+                throw new RuntimeException("ERROR: " + message);
+            } else {
+                System.out.println("WARNING: " + message);
+                System.exit(1);
+            }
+        }
+    }
+}
---
+++ src/com/xilinx/rapidwright/design/drc/NetRoutesThruLutAtMostOnce.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2021 Xilinx, Inc.
+ * All rights reserved.
+ *
+ * Author: Eddie Hung, Xilinx Research Labs.
+ *
+ * This file is part of RapidWright.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.xilinx.rapidwright.design.drc;
+
+import com.xilinx.rapidwright.design.Cell;
+import com.xilinx.rapidwright.design.Design;
+import com.xilinx.rapidwright.design.DesignTools;
+import com.xilinx.rapidwright.design.Net;
+import com.xilinx.rapidwright.design.SitePinInst;
+import com.xilinx.rapidwright.device.BELPin;
+import com.xilinx.rapidwright.device.Node;
+import com.xilinx.rapidwright.device.PIP;
+import com.xilinx.rapidwright.util.Pair;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class NetRoutesThruLutAtMostOnce implements DesignRuleCheckInterface {
+
+    private String lutName(Net net, Node node) {
+        String nodeWireName = node.getWireName();
+        return node.getTile().getName() + "/" + nodeWireName.substring(0, nodeWireName.length() - 1);
+    }
+
+    @Override
+    public int run(Design design) {
+        List<Pair<Net, List<Pair<String, Integer>>>> netToLutRoutethrus = design.getNets().stream()
+                .map((n) -> {
+                    Map<String, Integer> lutRoutethrus = new HashMap<>();
+
+                    for (PIP p : n.getPIPs()) {
+                        if (!p.isRouteThru()) continue;
+                        // Set to 1 if not exist, increment by 1 if does exist
+                        lutRoutethrus.merge(lutName(n, p.getStartNode()), 1, Integer::sum);
+                    }
+
+                    for (SitePinInst spi : n.getSinkPins()) {
+                        for (BELPin bp : spi.getSiteWireBELPins()) {
+                            Cell c = spi.getSiteInst().getCell(bp.getBEL());
+                            // Filter out all non routethru cells
+                            if (c == null || !c.isRoutethru()) continue;
+
+                            assert(DesignTools.isBELALut(bp.getBELName()));
+
+                            // Check that is pin is actually used by this BEL
+                            if (c.getLogicalPinMapping(bp.getName()) == null) continue;
+
+                            // Set to 1 if not exist, increment by 1 if does exist
+                            lutRoutethrus.merge(lutName(spi.getNet(), spi.getConnectedNode()), 1, Integer::sum);
+                        }
+                    }
+
+                    // Copy out all lutNames routed through just once
+                    List<Pair<String, Integer>> moreThanOneRoutethru = new ArrayList<>();
+                    for (Map.Entry<String, Integer> e : lutRoutethrus.entrySet()) {
+                        if (e.getValue() == 1) continue;
+                        moreThanOneRoutethru.add(new Pair<>(e.getKey(), e.getValue()));
+                    }
+
+                    return new Pair<>(n, moreThanOneRoutethru);
+                // Filter out all nets an empty list of more-than-one-routethru
+                }).filter(
+                        (e) -> !e.getSecond().isEmpty()
+                ).collect(Collectors.toList());
+
+        int numFails = 0;
+        for (Pair<Net, List<Pair<String, Integer>>> e : netToLutRoutethrus) {
+            for (Pair<String, Integer> l : e.getSecond()) {
+                System.out.println("Net '" + e.getFirst() + "' routes-thru this LUT more than once: " +
+                        l.getFirst() + "; this may not be faithfully representable to Vivado");
+                numFails += l.getSecond() - 1;
+            }
+        }
+        return numFails;
+    }
+}
