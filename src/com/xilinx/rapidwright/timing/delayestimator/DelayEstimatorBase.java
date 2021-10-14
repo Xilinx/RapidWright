f863d913388b34144ea88885d9aa1cce9347456e
pongstorn <pongsto@xilinx.com>
2021-10-14 11:37:05 -0700

    rename calcTimingGroupDelay  to calcNodeGroupDelay

--- src/com/xilinx/rapidwright/timing/delayestimator/DelayEstimatorBase.java
+++ src/com/xilinx/rapidwright/timing/delayestimator/DelayEstimatorBase.java
@@ -128,7 +128,7 @@ public class DelayEstimatorBase<T extends InterconnectInfo> implements java.io.S
             return inputSitePinDelay.getOrDefault(exitNode.getWireName(), (short) 0);
         }
 
-        return calcTimingGroupDelay(termInfo.ng, termInfo.begin(), termInfo.end(), 0d);
+        return calcNodeGroupDelay(termInfo.ng, termInfo.begin(), termInfo.end(), 0d);
     }
 
     /**
@@ -407,7 +407,7 @@ public class DelayEstimatorBase<T extends InterconnectInfo> implements java.io.S
      * @return delay of the tg. When useUTurnNodes was set to false, return Short.MAX_VALUE/2 if the node graph is a U-turn.
      *         to indicate the node graph should be ignored.
      */
-    short calcTimingGroupDelay(T.NodeGroupType tg, short begLoc, short endLoc, Double dly) {
+    short calcNodeGroupDelay(T.NodeGroupType tg, short begLoc, short endLoc, Double dly) {
         int size = (tg.orientation() == T.Orientation.HORIZONTAL) ? numCol : numRow;
         short d = 0;
         List<Short> dArray = distArrays.get(tg.orientation()).get(tg.type());
