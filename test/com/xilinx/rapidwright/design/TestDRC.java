1d04603003f898e05e1c1e96f819de11e102b624
eddieh-xlnx <eddieh-xlnx@users.noreply.github.com>
2021-10-15 11:13:00 -0700

    Add tests

---
+++ test/com/xilinx/rapidwright/design/TestDRC.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021 Xilinx, Inc.
+ * All rights reserved.
+ *
+ * Author: Eddie Hung, Xilinx Research Labs.
+ *
+ * This file is part of RapidWright.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.xilinx.rapidwright.design;
+
+import com.xilinx.rapidwright.checker.CheckOpenFiles;
+import com.xilinx.rapidwright.design.drc.NetRoutesThruLutAtMostOnce;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestDRC {
+    @Test
+    @CheckOpenFiles
+    public void testRoutethruPass() {
+        final String dcpPath = "RapidWrightDCP/routethru_luts.dcp";
+        Design design = Design.readCheckpoint(dcpPath);
+
+        NetRoutesThruLutAtMostOnce drc = new NetRoutesThruLutAtMostOnce();
+        Assertions.assertEquals(drc.run(design), 0);
+    }
+
+    @Test
+    @CheckOpenFiles
+    public void testRoutethruFail() {
+        // DCP derived from https://github.com/Xilinx/RapidWright/issues/226#issuecomment-906164846
+        // (specifically, the interchange input has been re-converted into a new DCP after
+        // PRs #253 and #254)
+
+        final String dcpPath = "RapidWrightDCP/bug226.dcp";
+        Design design = Design.readCheckpoint(dcpPath);
+
+        NetRoutesThruLutAtMostOnce drc = new NetRoutesThruLutAtMostOnce();
+        Assertions.assertEquals(drc.run(design), 4);
+    }
+
+}
