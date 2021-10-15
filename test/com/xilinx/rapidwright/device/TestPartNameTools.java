a2bc53d19f9f9aedb0f77ee7732fed122fdbad8f
eddieh-xlnx <eddieh-xlnx@users.noreply.github.com>
2021-10-15 15:57:28 -0700

    Add test from #73

--- test/com/xilinx/rapidwright/device/TestPartNameTools.java
+++ test/com/xilinx/rapidwright/device/TestPartNameTools.java
@@ -5,7 +5,17 @@ import org.junit.jupiter.api.Test;
 
 public class TestPartNameTools {
     @Test
-    public void test() {
+    public void testGetPartCase() {
         Assertions.assertEquals(PartNameTools.getPart("xcvu3p-ffvc1517-2-i"), PartNameTools.getPart("xcVu3P-ffVC1517-2-i"));
+    }
+
+    @Test
+    public void testXazu2egResources() {
+        Part p = PartNameTools.getPart("xazu2eg-sfvc784-1");
+        Assertions.assertEquals(p.getDsp(), 240);
+        Assertions.assertEquals(p.getBlockRams(), 150);
+        Assertions.assertEquals(p.getUltraRams(), 0);
+        Assertions.assertEquals(p.getLutElements(), 47232);
+        Assertions.assertEquals(p.getFlipflops(), 94464);
     }
 }
