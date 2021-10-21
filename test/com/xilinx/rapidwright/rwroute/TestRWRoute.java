d04afd8c2c4aaabcb33a20335f6da81f5ba8cd62
Yun Zhou <Yun.Zhou@ugent.be>
2021-10-21 21:23:49 +0200

    add tests for RWRoute
    Signed-off-by: Yun Zhou <Yun.Zhou@ugent.be>

---
+++ test/com/xilinx/rapidwright/rwroute/TestRWRoute.java
@@ -0,0 +1,37 @@
+package com.xilinx.rapidwright.rwroute;
+
+import org.junit.jupiter.api.Test;
+
+import com.xilinx.rapidwright.design.Design;
+
+public class TestRWRoute {
+	@Test
+	public void testNonTimingDrivenFullRouting() {
+		Design design = Design.readCheckpoint("RapidWrightDCP/bnn.dcp");
+		RWRoute.routeDesignFullNonTimingDriven(design);
+	}
+	
+	@Test
+	public void testTimingDrivenFullRouting() {
+//		Design design = Design.readCheckpoint("RapidWrightDCP/bnn.dcp");
+//		RWRoute.routeDesignFullTimingDriven(design);
+	}
+	
+	@Test
+	public void testNonTimingDrivenFullRoutingWithClk() {
+		Design design = Design.readCheckpoint("RapidWrightDCP/optical-flow.dcp");
+		RWRoute.routeDesignFullNonTimingDriven(design);
+	}
+	
+	@Test
+	public void testTimingDrivenFullRoutingWithClk() {
+//		Design design = Design.readCheckpoint("RapidWrightDCP/optical-flow.dcp");
+//		RWRoute.routeDesignWithUserDefinedArguments(design, new String[] {"--clkRouteTiming", "RapidWrightDCP/clkRouteTemplate_BUFGCE_X0Y58.txt", "--verbose"});
+	}
+	
+	@Test
+	public void testNonTimingDrivenPartialRouting() {
+		Design design = Design.readCheckpoint("RapidWrightDCP/picoblaze_partial.dcp");
+		RWRoute.routeDesignPartialNonTimingDriven(design);
+	}
+}
