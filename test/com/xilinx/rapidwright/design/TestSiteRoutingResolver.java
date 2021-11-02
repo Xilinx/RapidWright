2919f6a8db6142e017d4d478af02172d3730e419
Chris Lavin <clavin@xilinx.com>
2021-11-02 13:38:44 -0600

    Site routing resolver when combining in-context module designs.
    Signed-off-by: Chris Lavin <clavin@xilinx.com>

---
+++ test/com/xilinx/rapidwright/design/TestSiteRoutingResolver.java
@@ -0,0 +1,55 @@
+package com.xilinx.rapidwright.design;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.xilinx.rapidwright.device.BELPin;
+import com.xilinx.rapidwright.support.CheckOpenFiles;
+import com.xilinx.rapidwright.support.RapidWrightDCP;
+import com.xilinx.rapidwright.tests.CodePerfTracker;
+import com.xilinx.rapidwright.util.Pair;
+
+public class TestSiteRoutingResolver {
+
+    private Pair<String,String> inputSiteWire1 = new Pair<>("SLICE_X16Y238","A2"); 
+    
+    private Pair<String,String> inputSiteWire2 = new Pair<>("SLICE_X13Y237","F5");
+    
+    @SuppressWarnings("unchecked")
+    private Map<Pair<String,String>,String> mimicInContextInputPortNetSiteRouting(Design design) {
+        Map<Pair<String,String>,String> initialState = new HashMap<>();
+        
+        for(Pair<String,String> siteWire : new Pair[] {inputSiteWire1, inputSiteWire2}) {
+            SiteInst i = design.getSiteInstFromSiteName(siteWire.getFirst());
+            Net net = i.getNetFromSiteWire(siteWire.getSecond());
+            initialState.put(siteWire, net.getName());
+            BELPin pin = i.getSiteWirePins(siteWire.getSecond())[0];
+            i.unrouteIntraSiteNet(pin, pin);
+            i.routeIntraSiteNet(design.getVccNet(), pin, pin);
+        }
+        
+        return initialState;
+    }
+    
+    @Test
+    @CheckOpenFiles
+    public void testPicoblazeOOC() {
+        String dcpPath = RapidWrightDCP.getString("picoblaze_ooc_X10Y235.dcp");
+        Design design = Design.readCheckpoint(dcpPath, CodePerfTracker.SILENT);
+
+        // Convert DCP to introduce test scenario
+        Map<Pair<String,String>,String> initialSiteRoutes = mimicInContextInputPortNetSiteRouting(design);
+        
+        DesignTools.resolveSiteRoutingFromInContextPorts(design);
+        
+        for(Entry<Pair<String,String>,String> e : initialSiteRoutes.entrySet()) {
+            SiteInst i = design.getSiteInstFromSiteName(e.getKey().getFirst());
+            Net net = i.getNetFromSiteWire(e.getKey().getSecond());
+            Assertions.assertEquals(net.getName(), e.getValue());
+        }
+    }
+}
