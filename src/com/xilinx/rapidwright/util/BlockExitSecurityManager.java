f53982ad8c70e45a9c9ac401607c61b7d19b118d
Chris Lavin <chris.lavin@xilinx.com>
2021-11-05 13:13:00 -0700

    Catch System.exit() calls and throw an error instead for Python REPL (#284)
    * Catch System.exit() calls and throw an error instead for Python REPL
    
    Signed-off-by: Chris Lavin <clavin@xilinx.com>
    
    * Add Python tests to CI
    
    * Adding Python test for System.exit() blocker
    
    Signed-off-by: Chris Lavin <clavin@xilinx.com>
    
    * Update src/com/xilinx/rapidwright/util/BlockExitSecurityManager.java
    
    Co-authored-by: eddieh-xlnx <90657806+eddieh-xlnx@users.noreply.github.com>
    
    * Setting RAPIDWRIGHT_PATH for testPython
    
    Signed-off-by: Chris Lavin <clavin@xilinx.com>
    
    Co-authored-by: eddieh-xlnx <eddieh-xlnx@users.noreply.github.com>
    Co-authored-by: eddieh-xlnx <90657806+eddieh-xlnx@users.noreply.github.com>

---
+++ src/com/xilinx/rapidwright/util/BlockExitSecurityManager.java
@@ -0,0 +1,42 @@
+package com.xilinx.rapidwright.util;
+
+import java.security.Permission;
+
+/**
+ * A custom SecurityManager to enable catching System.exit() calls and throwing an exception instead.
+ * Since the SecurityManager is being deprecated in Java 17, it will not be used by default but can 
+ * be invoked by 'PythonSecurityManager.blockSystemExitCalls()'.    
+ * @see <a href="JEPS 411">https://openjdk.java.net/jeps/411</a>
+ * @see <a href="JDK-8199704">https://bugs.openjdk.java.net/browse/JDK-8199704</a>
+ */
+@SuppressWarnings("removal")
+public class BlockExitSecurityManager extends SecurityManager {
+    
+    private static BlockExitSecurityManager singleton = null; 
+    
+    @Override 
+    public void checkExit(int status) {
+        throw new SecurityException("JVM attempted to exit with status: " + status);
+    }
+
+    @Override 
+    public void checkPermission(Permission p) {
+    }
+
+    /**
+     * Blocks {@link System.exit()} calls from exiting the JVM and instead throws a 
+     * {@link SecurityException} in its place.  This is useful when running a JVM through an 
+     * interactive interpreter such as Python so that individual commands to not cause the REPL
+     * to exit. This will generate a WARNING message in Java 17, and must be explicitly allowed in 
+     * future JDK releases.
+     * 
+     * @see <a href="JEPS 411">https://openjdk.java.net/jeps/411</a>
+     * @see <a href="JDK-8199704">https://bugs.openjdk.java.net/browse/JDK-8199704</a>
+     */
+    public static void blockSystemExitCalls() {
+        if(singleton == null) {
+            singleton = new BlockExitSecurityManager();
+            System.setSecurityManager(singleton);            
+        }
+    }
+}
