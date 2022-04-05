f5fd40cadb53c288a871cac0897abce92d5556c7
Jakob Wenzel <jakobw@xilinx.com>
2022-04-05 14:42:34 +0200

    Include Library in EDIFCell's equals and HashCode
    Signed-off-by: Jakob Wenzel <jakobw@xilinx.com>

--- src/com/xilinx/rapidwright/edif/EDIFCell.java
+++ src/com/xilinx/rapidwright/edif/EDIFCell.java
@@ -34,6 +34,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Queue;
 
 /**
@@ -556,6 +557,20 @@ public class EDIFCell extends EDIFPropertyObject implements EDIFEnumerable {
 			}
 		}
 		return null;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		if (!super.equals(o)) return false;
+		EDIFCell edifCell = (EDIFCell) o;
+		return Objects.equals(library, edifCell.library);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(super.hashCode(), library);
 	}
 }
  
