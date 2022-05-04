e171e7908ec206139d8e20f8a5b0d2028c9c0613
Chris Lavin <clavin@xilinx.com>
2022-05-04 17:05:42 -0600

    Test LUT route-thru on intrasite routing (#394)
    * Test LUT route-thru on intrasite routing
    
    Signed-off-by: Chris Lavin <clavin@xilinx.com>
    
    * Refactors test to new test dir structure
    
    Signed-off-by: Chris Lavin <clavin@xilinx.com>

--- test/src/com/xilinx/rapidwright/design/TestSiteInst.java
+++ test/src/com/xilinx/rapidwright/design/TestSiteInst.java
@@ -32,8 +32,27 @@ import com.xilinx.rapidwright.device.BEL;
 import com.xilinx.rapidwright.device.BELPin;
 import com.xilinx.rapidwright.device.Device;
 import com.xilinx.rapidwright.device.Series;
+import com.xilinx.rapidwright.support.RapidWrightDCP;
 
 public class TestSiteInst {
+
+    @Test
+    public void testRouteIntraSiteNet() {
+        Design d = Design.readCheckpoint(RapidWrightDCP.getPath("picoblaze_ooc_X10Y235.dcp"));
+        SiteInst si = d.getSiteInstFromSiteName("SLICE_X15Y237");
+        Net net = d.createNet("dummy_test_net");
+        d.createAndPlaceCell("dummy_flop", Unisim.FDRE, "SLICE_X15Y237/EFF");
+        
+        BELPin src = si.getBEL("E3").getPin("E3");
+        BELPin snk = si.getBEL("EFF").getPin("D");
+        Assertions.assertTrue(si.routeIntraSiteNet(net, src, snk));
+        
+        Assertions.assertEquals(si.getSiteWiresFromNet(net).size(), 3);
+        
+        Assertions.assertTrue(si.unrouteIntraSiteNet(src, snk));
+        
+        Assertions.assertEquals(si.getSiteWiresFromNet(net).size(), 0);
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {Device.KCU105, Device.PYNQ_Z1})
