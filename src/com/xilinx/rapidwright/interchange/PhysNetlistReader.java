dc3ea688a6307e1f13a925ed9f8a6abc8382e520
Chris Lavin <clavin@xilinx.com>
2022-08-19 17:22:24 -0600

    Update src/com/xilinx/rapidwright/interchange/PhysNetlistReader.java

--- src/com/xilinx/rapidwright/interchange/PhysNetlistReader.java
+++ src/com/xilinx/rapidwright/interchange/PhysNetlistReader.java
@@ -299,8 +299,10 @@ public class PhysNetlistReader {
                     if(c.getBEL().getPin(belPinName) == null) {
                         System.err.println("WARNING: On cell " + c.getName() + ", a logical pin '" +
                                 c.getType() + "." + cellPinName + "' is being mapped on to a BEL pin '" 
-                                + c.getBELName() + "." + belPinName + "' that does not exist.");
+                                + c.getBELName() + "." + belPinName + "' that does not exist. " 
+                                + "This may result in an invalid design.");
                     }
+
                     c.addPinMapping(belPinName, cellPinName);
                     if(pinMapping.getIsFixed()) {
                         c.fixPin(belPinName);
