e2abf4bb20e6347783cf016dcb52675d0c4c2b2b
Eddie Hung <eddie.hung@amd.com>
2022-09-27 10:15:00 -0700

    Test Net.removePin() on static nets
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>
    
    Conflicts:
    	test/src/com/xilinx/rapidwright/design/TestDesignTools.java

--- test/src/com/xilinx/rapidwright/design/TestNet.java
+++ test/src/com/xilinx/rapidwright/design/TestNet.java
@@ -133,4 +133,24 @@ public class TestNet {
         net.removePin(altSrc);
         Assertions.assertNull(net.getAlternateSource());
     }
+
+    @Test
+    public void testRemovePinOnStaticNet() {
+        Design design = new Design("test", Device.KCU105);
+        SiteInst si = design.createSiteInst(design.getDevice().getSite("SLICE_X0Y0"));
+        Net gndNet = design.getGndNet();
+        SitePinInst a6 = gndNet.createPin("A6", si);
+        SitePinInst b6 = gndNet.createPin("B6", si);
+        TestDesignTools.addPIPs(gndNet, new String[]{
+                "INT_X0Y0/INT.LOGIC_OUTS_E29->>INT_NODE_SINGLE_DOUBLE_101_INT_OUT",
+                "INT_X0Y0/INT.INT_NODE_SINGLE_DOUBLE_101_INT_OUT->>SS1_E_BEG7",
+                "INT_X0Y0/INT.INT_NODE_IMUX_64_INT_OUT->>IMUX_E16",
+                "INT_X0Y0/INT.NN1_E_END0->>INT_NODE_IMUX_64_INT_OUT",
+                "INT_X0Y0/INT.INT_NODE_IMUX_64_INT_OUT->>IMUX_E17"
+        });
+        gndNet.removePin(a6, true);
+        Assertions.assertEquals(gndNet.getPIPs().size(), 4);
+        gndNet.removePin(b6, true);
+        Assertions.assertEquals(gndNet.getPIPs().size(), 0);
+    }
 }
