9c879c1312edd5825ab8fb902efdb9df4cb497de
Chris Lavin <chris.lavin@amd.com>
2022-10-12 17:58:32 -0600

    Test for #558
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- test/src/com/xilinx/rapidwright/rwroute/TestRouterHelper.java
+++ test/src/com/xilinx/rapidwright/rwroute/TestRouterHelper.java
@@ -26,7 +26,9 @@ import com.xilinx.rapidwright.design.Design;
 import com.xilinx.rapidwright.design.Net;
 import com.xilinx.rapidwright.design.SiteInst;
 import com.xilinx.rapidwright.design.SitePinInst;
+import com.xilinx.rapidwright.device.Node;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -171,5 +173,31 @@ public class TestRouterHelper {
                         "CIN,false"
                 ))
         );
+    }
+
+    @Test
+    public void testProjectOutputPins() {
+        Design d = new Design("test", "xcvu19p-fsva3824-1-e");
+
+        String[] testSites = { "SLICE_X0Y1199", "SLICE_X1Y1199" };
+        for (String siteName : testSites) {
+            SiteInst si = d.createSiteInst(siteName);
+            for (String pinName : si.getSitePinNames()) {
+                SitePinInst pin = new SitePinInst(pinName, si);
+                // Only test output pins to project
+                if (!pin.isOutPin() || pin.getName().equals("COUT")) {
+                    continue;
+                }
+
+                Node intNode = RouterHelper.projectOutputPinToINTNode(pin);
+                Assertions.assertNotNull(intNode);
+            }
+        }
+
+        SiteInst si = d.createSiteInst(d.getDevice().getSite("BITSLICE_RX_TX_X1Y78"));
+        SitePinInst p = new SitePinInst("TX_T_OUT", si);
+        Node intNode = RouterHelper.projectOutputPinToINTNode(p);
+        Assertions.assertNotNull(intNode);
+        Assertions.assertEquals(intNode.toString(), "INT_INTF_L_CMT_X182Y90/LOGIC_OUTS_R19");
     }
 }
