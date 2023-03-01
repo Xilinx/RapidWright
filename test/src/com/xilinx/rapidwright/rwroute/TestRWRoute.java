354d144d8df55dde0438ec0121e0e1078f33c81a
Eddie Hung <eddie.hung@amd.com>
2023-03-01 13:45:42 -0800

    Remove unused import
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- test/src/com/xilinx/rapidwright/rwroute/TestRWRoute.java
+++ test/src/com/xilinx/rapidwright/rwroute/TestRWRoute.java
@@ -37,7 +37,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
