b3e47149d6a53920825bc4a8a38e4735d70f5f74
Eddie Hung <eddie.hung@amd.com>
2023-03-01 11:16:58 -0800

    PipelineGenerator test
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- test/src/com/xilinx/rapidwright/examples/TestExamples.java
+++ test/src/com/xilinx/rapidwright/examples/TestExamples.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.Test;
 
 public class TestExamples {
     @Test
-    public void testPipelineGeneratorWithRoutign() {
+    public void testPipelineGenerator() {
+        PipelineGenerator.main(new String[]{});
+    }
+
+    @Test
+    public void testPipelineGeneratorWithRouting() {
         PipelineGeneratorWithRouting.main(new String[]{});
     }
 }
