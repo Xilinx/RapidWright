0ceaf6b6609cf9832377495250792e10657a42ff
Chris Lavin <chris.lavin@amd.com>
2023-05-02 12:54:17 -0600

    More compact data file, all family coverage with comprehensive test (#643)
    * More compact data file, all family coverage with comprehensive test
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Skip odd families for now
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Add support timeline
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    ---------
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- src/com/xilinx/rapidwright/util/DataVersions.java
+++ src/com/xilinx/rapidwright/util/DataVersions.java
@@ -326,6 +326,6 @@ public class DataVersions {
         dataVersionMap.put("data/devices/zynquplusrfsoc/xqzu49dr_db.dat", new Pair<>("xqzu49dr-db-dat", "df942c4f499805206466b4d89813c235"));
         dataVersionMap.put("data/partdump.csv", new Pair<>("partdump-csv", "f0417647e80cb1de13c65f0a8973df17"));
         dataVersionMap.put("data/parts.db", new Pair<>("parts-db", "b728320ae4d94f28402e03343363b6e8"));
-        dataVersionMap.put("data/unisim_data.dat", new Pair<>("unisim-data-dat", "70faaac529fd20e64a0acaa4827a885e"));
+        dataVersionMap.put("data/unisim_data.dat", new Pair<>("unisim-data-dat", "10d1c6e79b176e329019d7bc752ca301"));
     }
 }
