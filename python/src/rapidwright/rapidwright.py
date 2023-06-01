4e03237a76a0eeb8cb6a229830d59de54e2e64eb
Chris Lavin <chris.lavin@amd.com>
2023-06-01 17:36:56 -0600

    Bump Python to 2023.1.0, update to license_files
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- python/src/rapidwright/rapidwright.py
+++ python/src/rapidwright/rapidwright.py
@@ -24,7 +24,7 @@ from jpype.types import *
 from typing import List, Optional
 import os, urllib.request, platform
 
-version='2022.2.3'
+version='2023.1.0'
 
 def start_jvm():
     os_str = 'lin64'
