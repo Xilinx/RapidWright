a2b12b63538b538038d9417370cd528c50275edf
Chris Lavin <chris.lavin@amd.com>
2023-06-02 12:33:14 -0600

    Fix bat file directory change
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- bin/rapidwright.bat
+++ bin/rapidwright.bat
@@ -21,7 +21,7 @@ REM  limitations under the License.
 REM Check that the main jar has been built
 REM NOTE: Does not check that it is up-to-date
 SET "BAT_SOURCE=%~dp0"
-SET "MAIN_JAR=%BAT_SOURCE%build\libs\main.jar"
+SET "MAIN_JAR=%BAT_SOURCE:~0,-4%\build\libs\main.jar"
 IF EXIST %MAIN_JAR% GOTO JAR_EXISTS
 echo "RapidWright not yet compiled. Please run './gradlew compileJava' from '%BAT_SOURCE%'"
 EXIT /B 1
