c6523b2442361916b4994ea36fadb42fe95808fe
Eddie Hung <eddie.hung@amd.com>
2023-07-17 14:06:45 -0700

    Require GlobalSignalRouting.getLCBMappings() with getNodeStatus
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- src/com/xilinx/rapidwright/rwroute/GlobalSignalRouting.java
+++ src/com/xilinx/rapidwright/rwroute/GlobalSignalRouting.java
@@ -96,7 +96,7 @@ public class GlobalSignalRouting {
         }
         clk.setPIPs(clkPIPs);
 
-        Map<RouteNode, List<SitePinInst>> lcbMappings = getLCBPinMappings(clk);
+        Map<RouteNode, List<SitePinInst>> lcbMappings = getLCBPinMappings(clk.getPins(), getNodeStatus);
 
         UltraScaleClockRouting.routeToLCBs(clk, getStartingPoint(horDistributionLines, device), lcbMappings.keySet());
 
@@ -204,7 +204,7 @@ public class GlobalSignalRouting {
         List<RouteNode> downLines = UltraScaleClockRouting.routeToHorizontalDistributionLines(clk, vrouteDown, downClockRegions, true, getNodeStatus);//TODO this is where the antenna node shows up
         if (downLines != null) upDownDistLines.addAll(downLines);
 
-        Map<RouteNode, List<SitePinInst>> lcbMappings = getLCBPinMappings(clk);
+        Map<RouteNode, List<SitePinInst>> lcbMappings = getLCBPinMappings(clk.getPins(), getNodeStatus);
         UltraScaleClockRouting.routeDistributionToLCBs(clk, upDownDistLines, lcbMappings.keySet());
 
         UltraScaleClockRouting.routeLCBsToSinks(clk, lcbMappings, getNodeStatus);
@@ -238,15 +238,6 @@ public class GlobalSignalRouting {
                 downClockRegions.add(cr);
             }
         }
-    }
-
-    /**
-     * Maps each sink SitePinInsts of a clock net to a leaf clock buffer node.
-     * @param clk The clock net in question.
-     * @return A map between leaf clock buffer nodes and sink SitePinInsts.
-     */
-    public static Map<RouteNode, List<SitePinInst>> getLCBPinMappings(Net clk) {
-        return getLCBPinMappings(clk.getPins(), (n) -> NodeStatus.AVAILABLE);
     }
 
     /**
