2219fe53f8318806f186d764974fcddea11f351b
eddieh-xlnx <eddie.hung@amd.com>
2023-08-23 10:41:52 -0700

    [PhysNetlistWriter] Fix route tree construction for bidir PIPs (#791)
    * Test fully routed clock nets (many bidir PIPs) have no stubs
    
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>
    
    * [PhysNetlistWriter] Fix route tree construction for bidir PIPs
    
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>
    
    * Add more clock nets
    
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>
    
    * Restore RapidWrightDCP
    
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>
    
    ---------
    
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- test/src/com/xilinx/rapidwright/interchange/TestPhysNetlistWriter.java
+++ test/src/com/xilinx/rapidwright/interchange/TestPhysNetlistWriter.java
@@ -187,4 +187,35 @@ public class TestPhysNetlistWriter {
             break;
         }
     }
+
+    @Test
+    public void testReversedPIPs(@TempDir Path tempDir) throws IOException {
+        Design design = RapidWrightDCP.loadDCP("microblazeAndILA_3pblocks.dcp");
+
+        String interchangePath = tempDir.resolve("design.phys").toString();
+        PhysNetlistWriter.writePhysNetlist(design, interchangePath);
+
+        ReaderOptions rdOptions =
+                new ReaderOptions(ReaderOptions.DEFAULT_READER_OPTIONS.traversalLimitInWords * 64,
+                        ReaderOptions.DEFAULT_READER_OPTIONS.nestingLimit * 128);
+        MessageReader readMsg = Interchange.readInterchangeFile(interchangePath, rdOptions);
+
+        PhysNetlist.Reader physNetlist = readMsg.getRoot(PhysNetlist.factory);
+
+        List<String> allStrings = PhysNetlistReader.readAllStrings(physNetlist);
+
+        for (PhysNet.Reader net : physNetlist.getPhysNets()) {
+            String netName = allStrings.get(net.getName());
+            // It's known that all these clock nets are fully routed
+            if (!netName.equals("base_mb_i/clk_wiz_1/inst/clk_out1") &&
+                !netName.equals("base_mb_i/mdm_1/U0/No_Dbg_Reg_Access.BUFG_DRCK/Dbg_Clk_31") &&
+                !netName.equals("base_mb_i/clk_wiz_1/inst/clkfbout_buf_base_mb_clk_wiz_1_0") &&
+                !netName.equals("dbg_hub/inst/BSCANID.u_xsdbm_id/itck_i")) {
+                continue;
+            }
+
+            Assertions.assertEquals(1, net.getSources().size());
+            Assertions.assertEquals(0, net.getStubs().size());
+        }
+    }
 }
