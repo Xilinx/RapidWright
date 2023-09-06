4c70977fbfa37169b3a393bde3779acca4b2cf6e
Chris Lavin <chris.lavin@amd.com>
2023-09-06 11:44:51 -0600

    Adds site pins to example code generation for nets. (#807)
    * Adds site pins to example code generation for nets.
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Update test to reflect changes
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Update test/src/com/xilinx/rapidwright/util/TestCodeGenerator.java
    
    Co-authored-by: eddieh-xlnx <eddie.hung@amd.com>
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    ---------
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    Co-authored-by: eddieh-xlnx <eddie.hung@amd.com>

--- test/shared/com/xilinx/rapidwright/design/TestDesignHelper.java
+++ test/shared/com/xilinx/rapidwright/design/TestDesignHelper.java
@@ -22,21 +22,15 @@
 
 package com.xilinx.rapidwright.design;
 
-import com.xilinx.rapidwright.device.Device;
+import com.xilinx.rapidwright.util.CodeGenerator;
 
 public class TestDesignHelper {
 
     public static Net createTestNet(Design design, String netName, String[] pips) {
-        Net net = design.createNet(netName);
-        TestDesignHelper.addPIPs(net, pips);
-        return net;
+        return CodeGenerator.createTestNet(design, netName, pips);
     }
 
     public static void addPIPs(Net net, String[] pips) {
-        Device device = net.getDesign().getDevice();
-        for (String pip : pips) {
-            net.addPIP(device.getPIP(pip));
-        }
+        CodeGenerator.addPIPs(net, pips);
     }
-
 }
