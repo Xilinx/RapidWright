7474db9d9ecf68349a6cff49613a110dbb1a1199
Eddie Hung <eddie.hung@amd.com>
2023-11-20 14:42:36 -0800

    Remove unused import
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- src/com/xilinx/rapidwright/rwroute/RouteNodeGraph.java
+++ src/com/xilinx/rapidwright/rwroute/RouteNodeGraph.java
@@ -24,7 +24,6 @@
 package com.xilinx.rapidwright.rwroute;
 
 import com.xilinx.rapidwright.design.Design;
-import com.xilinx.rapidwright.design.DesignTools;
 import com.xilinx.rapidwright.design.Net;
 import com.xilinx.rapidwright.design.SitePinInst;
 import com.xilinx.rapidwright.device.Device;
