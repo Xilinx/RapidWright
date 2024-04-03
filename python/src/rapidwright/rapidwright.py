1641ee0c3e49e640de8393b35dcdc807d9c0e872
Chris Lavin <chris.lavin@amd.com>
2024-04-03 12:08:27 -0600

    Bump Python version 2023.2.2
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- python/src/rapidwright/rapidwright.py
+++ python/src/rapidwright/rapidwright.py
@@ -24,7 +24,7 @@ from jpype.types import *
 from typing import List, Optional
 import os, urllib.request, platform
 
-version='2023.2.1'
+version='2023.2.2'
 
 def start_jvm():
     os_str = 'lin64'
