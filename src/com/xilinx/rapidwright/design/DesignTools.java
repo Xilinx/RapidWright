b96fa4215bacbbfdfe05f78f204fb2cdd94b166c
Chris Lavin <chris.lavin@amd.com>
2024-06-27 14:42:32 -0600

    [DesignTools.makeBlackBox()] Fixes an issue of removing CARRY blocks fed by routethrus (#1009)
    * Fix null netlist pointer on expanded macro children
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Fixes an issue of removing CARRY blocks fed by routethrus
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Update src/com/xilinx/rapidwright/design/DesignTools.java
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Update src/com/xilinx/rapidwright/design/DesignTools.java
    
    Co-authored-by: eddieh-xlnx <eddie.hung@amd.com>
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    ---------
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    Co-authored-by: eddieh-xlnx <eddie.hung@amd.com>

--- src/com/xilinx/rapidwright/design/DesignTools.java
+++ src/com/xilinx/rapidwright/design/DesignTools.java
@@ -1569,7 +1569,11 @@ public class DesignTools {
                                     String otherPinName = otherCell.getPinMappingsP2L().keySet().iterator().next();
                                     otherPin = pin.getBEL().getPin(otherPinName);
                                 } else {
-                                    otherPin = LUTTools.getLUTOutputPin(pin.getBEL());
+                                    // Make sure we are coming in on the routed-thru pin
+                                    String otherPinName = otherCell.getPinMappingsP2L().keySet().iterator().next();
+                                    if (pin.getName().equals(otherPinName)) {
+                                        otherPin = LUTTools.getLUTOutputPin(pin.getBEL());
+                                    }
                                 }
                                 if (otherPin != null) {
                                     Net otherNet = siteInst.getNetFromSiteWire(otherPin.getSiteWireName());
@@ -1738,15 +1742,28 @@ public class DesignTools {
         t.stop().start("Remove p&r");
 
         List<EDIFHierCellInst> allLeafs = d.getNetlist().getAllLeafDescendants(hierarchicalCell);
-
-        // Remove all placement and routing information related to the cell to be
-        // blackboxed
+        Set<Cell> cells = new HashSet<>();
         for (EDIFHierCellInst i : allLeafs) {
             // Get the physical cell, make sure we can unplace/unroute it first
             Cell c = d.getCell(i.getFullHierarchicalInstName());
             if (c == null) {
                 continue;
             }
+            cells.add(c);
+        }
+        // Find encrypted cells that need to be removed
+        if (d.getNetlist().getEncryptedCells().size() > 0) { 
+            String name = hierarchicalCell.getFullHierarchicalInstName();
+            for (Cell c : d.getCells()) {
+                if (c.getName().startsWith(name)) {
+                    cells.add(c);
+                }
+            }
+        }
+
+        // Remove all placement and routing information related to the cell to be
+        // blackboxed
+        for (Cell c : cells) {
             BEL bel = c.getBEL();
             SiteInst si = c.getSiteInst();
 
