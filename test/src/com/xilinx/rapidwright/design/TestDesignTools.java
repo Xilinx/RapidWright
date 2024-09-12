72e30eaecbc9ebb5f31143261a058d76739476e8
Eddie Hung <eddie.hung@amd.com>
2024-09-11 20:32:02 -0700

    [TestDesignTools] More extensive testing
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- test/src/com/xilinx/rapidwright/design/TestDesignTools.java
+++ test/src/com/xilinx/rapidwright/design/TestDesignTools.java
@@ -35,6 +35,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.xilinx.rapidwright.device.BEL;
 import com.xilinx.rapidwright.edif.EDIFHierPortInst;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -1003,42 +1004,73 @@ public class TestDesignTools {
     @ParameterizedTest
     @CsvSource({
             // US+
-            Device.AWS_F1+",SLICE_X0Y0/AFF,SRST1,true",
-            Device.AWS_F1+",SLICE_X0Y0/AFF2,SRST1,false",
-            Device.AWS_F1+",SLICE_X1Y1/HFF,SRST2,true",
-            Device.AWS_F1+",SLICE_X1Y1/HFF2,SRST2,false",
+            Device.AWS_F1+",SLICE_X0Y0/AFF,true",
+            Device.AWS_F1+",SLICE_X0Y0/AFF2,false",
+            Device.AWS_F1+",SLICE_X1Y1/HFF,true",
+            Device.AWS_F1+",SLICE_X1Y1/HFF2,false",
             // US
-            Device.KCU105+",SLICE_X0Y0/AFF,SRST_B1,true",
-            Device.KCU105+",SLICE_X0Y0/AFF2,SRST_B1,false",
-            Device.KCU105+",SLICE_X1Y1/HFF,SRST_B2,true",
-            Device.KCU105+",SLICE_X1Y1/HFF2,SRST_B2,false",
+            Device.KCU105+",SLICE_X0Y0/AFF,true",
+            Device.KCU105+",SLICE_X0Y0/AFF2,false",
+            Device.KCU105+",SLICE_X1Y1/HFF,true",
+            Device.KCU105+",SLICE_X1Y1/HFF2,false",
             // Series7
-            Device.PYNQ_Z1+",SLICE_X0Y0/AFF,SR,true",
-            Device.PYNQ_Z1+",SLICE_X0Y0/A5FF,SR,false",
-            Device.PYNQ_Z1+",SLICE_X1Y1/DFF,SR,true",
-            Device.PYNQ_Z1+",SLICE_X1Y1/D5FF,SR,false",
+            Device.PYNQ_Z1+",SLICE_X0Y0/AFF,true",
+            Device.PYNQ_Z1+",SLICE_X0Y0/A5FF,false",
+            Device.PYNQ_Z1+",SLICE_X1Y1/DFF,true",
+            Device.PYNQ_Z1+",SLICE_X1Y1/D5FF,false",
+            // Versal
+            "xcvc1902,SLICE_X40Y0/AFF,false",
+            "xcvc1902,SLICE_X40Y0/DFF2,false",
+            "xcvc1902,SLICE_X40Y0/EFF,false",
+            "xcvc1902,SLICE_X40Y0/HFF2,false",
+            "xcvp1002,SLICE_X40Y0/AFF,false",
+            "xcvp1002,SLICE_X40Y0/DFF2,false",
+            "xcvp1002,SLICE_X40Y0/EFF,false",
+            "xcvp1002,SLICE_X40Y0/HFF2,false",
     })
-    public void testCreateCeSrRstPinsToVCC(String deviceName, String location, String sitePinName, boolean connectGnd) {
+    public void testCreateCeSrRstPinsToVCC(String deviceName, String location, boolean connectSrToGnd) {
         Design design = new Design("test", deviceName);
         Cell c = design.createAndPlaceCell("ff", Unisim.FDRE, location);
-        BELPin sr = c.getBEL().getPin("SR");
         SiteInst si = c.getSiteInst();
+
+        BEL bel = c.getBEL();
+        BELPin ce = bel.getPin("CE");
+        Assertions.assertNull(si.getNetFromSiteWire(ce.getSiteWireName()));
+
+        BELPin sr = bel.getPin("SR");
         Assertions.assertNull(si.getNetFromSiteWire(sr.getSiteWireName()));
-        if (connectGnd) {
+        if (connectSrToGnd) {
             Net gnd = design.getGndNet();
             Assertions.assertTrue(si.routeIntraSiteNet(gnd, sr, sr));
         }
 
         DesignTools.createCeSrRstPinsToVCC(design);
 
-        SitePinInst spi = si.getSitePinInst(sitePinName);
-        if (design.getDevice().getSeries() == Series.Series7) {
-            // Nothing done for Series7
-            Assertions.assertNull(spi);
+        Series series = design.getDevice().getSeries();
+        Map<String, Pair<String, String>> pinMapping = DesignTools.belTypeSitePinNameMapping.get(series);
+        Pair<String, String> sitePinNames = pinMapping.get(bel.getName());
+        String ceSitePinName = sitePinNames.getFirst();
+        String srSitePinName = sitePinNames.getSecond();
+
+        Net vcc = design.getVccNet();
+        SitePinInst ceSpi = si.getSitePinInst(ceSitePinName);
+        if (series == Series.Series7) {
+            // Series7 have {CE,SR}USEDMUX which is used to supply VCC and GND respectively from inside the site
+            Assertions.assertNull(ceSpi);
         } else {
-            Assertions.assertNotNull(spi);
-            Net vcc = design.getVccNet();
-            Assertions.assertEquals(vcc, spi.getNet());
+            Assertions.assertNotNull(ceSpi);
+            Assertions.assertEquals(vcc, ceSpi.getNet());
+        }
+
+        SitePinInst srSpi = si.getSitePinInst(srSitePinName);
+        if (series == Series.Series7) {
+            // Series7 have {CE,SR}USEDMUX which is used to supply VCC and GND respectively from inside the site
+            Assertions.assertNull(srSpi);
+        } else if (series == Series.Versal) {
+            // FIXME
+        } else {
+            Assertions.assertNotNull(srSpi);
+            Assertions.assertEquals(vcc, srSpi.getNet());
         }
     }
 
