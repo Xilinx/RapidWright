26e1b65a0525c91ded6beda936e768e09ce6531c
Eddie Hung <eddie.hung@amd.com>
2024-10-09 16:06:08 -0700

    [RWRoute] addStaticNetRoutingTargets() to remove all static SPIs
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- src/com/xilinx/rapidwright/rwroute/RWRoute.java
+++ src/com/xilinx/rapidwright/rwroute/RWRoute.java
@@ -493,6 +493,9 @@ public class RWRoute {
         List<SitePinInst> sinks = staticNet.getSinkPins();
         if (sinks.size() > 0) {
             staticNet.unroute();
+            // Remove all output pins from unouted net as those used will be repopulated
+            staticNet.getPins().removeIf(SitePinInst::isOutPin);
+
             // Preserve all pins (e.g. in case of BOUNCE nodes that may serve as a site pin)
             preserveNet(staticNet, true);
             staticNetAndRoutingTargets.put(staticNet, sinks);
