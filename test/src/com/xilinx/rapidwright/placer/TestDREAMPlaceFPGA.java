c822bf93178d383e3934524b2b3cd9493fcc6944
Eddie Hung <eddie.hung@amd.com>
2024-10-25 19:17:35 -0700

    Add and use DREAMPlaceFPGA.isDREAMPlaceFPGAOnPath()
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- test/src/com/xilinx/rapidwright/placer/TestDREAMPlaceFPGA.java
+++ test/src/com/xilinx/rapidwright/placer/TestDREAMPlaceFPGA.java
@@ -29,6 +29,7 @@ import com.xilinx.rapidwright.support.RapidWrightDCP;
 import com.xilinx.rapidwright.util.ReportRouteStatusResult;
 import com.xilinx.rapidwright.util.VivadoTools;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -40,6 +41,9 @@ public class TestDREAMPlaceFPGA {
 
     @Test
     public void testDREAMPlaceFPGAMain(@TempDir Path tempDir) throws IOException {
+        // Skip test if dreamplacefpga is not on PATH
+        Assumptions.assumeTrue(DREAMPlaceFPGA.isDREAMPlaceFPGAOnPath());
+
         String inputDcp = RapidWrightDCP.getString("gnl_2_4_3_1.3_gnl_3000_07_3_80_80_placed.dcp");
         String outputDcp = tempDir.resolve("output.dcp").toString();
         DREAMPlaceFPGA.main(new String[]{inputDcp, outputDcp, DREAMPlaceFPGA.MAKE_DCP_OUT_OF_CONTEXT, tempDir.toString()});
@@ -52,6 +56,9 @@ public class TestDREAMPlaceFPGA {
 
     @Test
     public void testDREAMPlaceFPGA(@TempDir Path tempDir) throws IOException {
+        // Skip test if dreamplacefpga is not on PATH
+        Assumptions.assumeTrue(DREAMPlaceFPGA.isDREAMPlaceFPGAOnPath());
+
         boolean skipXdef = true;
         Design design = RapidWrightDCP.loadDCP("gnl_2_4_3_1.3_gnl_3000_07_3_80_80_placed.dcp", skipXdef);
         Assertions.assertTrue(design.getUsedSites().isEmpty());
