075298eba9f7cb9998a7c58007ce312a03754985
Eddie Hung <eddie.hung@amd.com>
2024-12-23 18:40:28 -0800

    Fix broken expected result!
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- test/src/com/xilinx/rapidwright/design/TestDesignTools.java
+++ test/src/com/xilinx/rapidwright/design/TestDesignTools.java
@@ -1446,7 +1446,7 @@ public class TestDesignTools {
         {
             DesignTools.createMissingSitePinInsts(design, design.getNet("clk"));
             SitePinInst spi = si.getSitePinInst("CLK2");
-            Assertions.assertEquals("[EFF.CLK, EFF2.CLK, FFF.CLK, FFF2.CLK, GFF.CLK, GFF2.CLK, HFF.CLK, HFF2.CLK]",
+            Assertions.assertEquals("[EFF.CLK, FFF.CLK, FFF2.CLK, GFF.CLK, GFF2.CLK, HFF.CLK, HFF2.CLK]",
                     DesignTools.getConnectedBELPins(spi).stream().map(BELPin::toString).sorted().collect(Collectors.toList()).toString());
         }
     }
