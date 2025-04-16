de19f4a243c3ae4897a896272e75218f71255bcd
Eddie Hung <eddie.hung@amd.com>
2025-04-15 17:10:43 -0700

    [NetTools] Transfer and output locked node state
    Signed-off-by: Eddie Hung <eddie.hung@amd.com>

--- src/com/xilinx/rapidwright/design/NetTools.java
+++ src/com/xilinx/rapidwright/design/NetTools.java
@@ -89,7 +89,8 @@ public class NetTools {
             sb.append("    ");
             sb.append(subtreeStart ? "[" : " ");
             sb.append(branchStart ? "{" : " ");
-            sb.append("   ");
+            sb.append(isArcLocked() ? "*" : " ");
+            sb.append("  ");
             boolean notFirstTimeVisitingThisMultiplyDrivenNode = multiplyDriven && !multiplyDrivenNodesVisited.add(this);
             boolean branchEnd = (branchEndIfNoFanouts && fanouts.isEmpty()) || notFirstTimeVisitingThisMultiplyDrivenNode;
             sb.append(branchEnd ? "}" : " ");
@@ -152,6 +153,9 @@ public class NetTools {
                 }
                 return v;
             });
+            if (pip.isPIPFixed()) {
+                endNode.setArcLocked(true);
+            }
             startNode.addFanout(endNode);
             if (!pip.isBidirectional()) {
                 if ((net.getType() == NetType.GND && startNode.isTiedToGnd()) ||
