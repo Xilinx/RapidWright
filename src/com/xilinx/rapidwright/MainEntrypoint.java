ea4094e9365843ef38c5ce9c5c79d755daabd2e3
Chris Lavin <chris.lavin@amd.com>
2025-06-03 12:56:00 -0700

    Regroup Instances -- command line tool (#1201)
    * Regroup Instances -- command line tool
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Adding a warning if design is implemented
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Adding a note
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    ---------
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- src/com/xilinx/rapidwright/MainEntrypoint.java
+++ src/com/xilinx/rapidwright/MainEntrypoint.java
@@ -38,6 +38,7 @@ import com.xilinx.rapidwright.design.blocks.PBlock;
 import com.xilinx.rapidwright.design.blocks.PBlockGenerator;
 import com.xilinx.rapidwright.design.merge.MergeDesigns;
 import com.xilinx.rapidwright.design.tools.LUTTools;
+import com.xilinx.rapidwright.design.tools.RegroupInstances;
 import com.xilinx.rapidwright.design.tools.RelocationTools;
 import com.xilinx.rapidwright.device.IntentCode;
 import com.xilinx.rapidwright.device.PseudoPIPHelper;
@@ -191,6 +192,7 @@ public class MainEntrypoint {
         addFunction("PrintEDIFInstances", PrintEDIFInstances::main);
         addFunction("ProbeRouter", ProbeRouter::main);
         addFunction("PseudoPIPHelper", PseudoPIPHelper::main);
+        addFunction("RegroupInstances", RegroupInstances::main);
         addFunction("RelocationTools", RelocationTools::main);
         addFunction("ReplaceEDIFInDCP", ReplaceEDIFInDCP::main);
         addFunction("ReportDevicePerformance", ReportDevicePerformance::main);
