cabb5e46afc50f7872cfe7891bfeee1f7bda7d65
Chris Lavin <chris.lavin@amd.com>
2025-10-21 15:18:38 -0600

    Adds encrypted cells check flag; hasEncryptedCells()
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- src/com/xilinx/rapidwright/edif/EDIFTools.java
+++ src/com/xilinx/rapidwright/edif/EDIFTools.java
@@ -1097,24 +1097,8 @@ public class EDIFTools {
         try {
             ensureCorrectPartInEDIF(edif, partName);
             edif.exportEDIF(out);
-            if (dcpFileName != null && edif.getEncryptedCells() != null) {
-                if (edif.getEncryptedCells().size() > 0) {
-                    // Verify that at least one of the edn files collected are actually in the design
-                    boolean verifiedEncryptedCell = false;
-                    for (String edn : edif.getEncryptedCells()) {
-                        int start = edn.lastIndexOf(File.separator);
-                        int end = edn.lastIndexOf(".edn");
-                        String cellName = edn.substring(start == -1 ? 0 : start + 1, end);
-                        EDIFCell cell = edif.getCell(cellName);
-                        if (cell != null && cell.isLeafCellOrBlackBox()) {
-                            verifiedEncryptedCell = true;
-                            break;
-                        }
-                    }
-                    if (verifiedEncryptedCell) {
-                        writeTclLoadScriptForPartialEncryptedDesigns(edif, dcpFileName, partName);
-                    }
-                }
+            if (dcpFileName != null && edif.hasEncryptedCells()) {
+                writeTclLoadScriptForPartialEncryptedDesigns(edif, dcpFileName, partName);
             }
         } catch (IOException e) {
             e.printStackTrace();
