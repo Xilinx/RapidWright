2f1d9b362ac6fd783a792acac41b86a8c91a052b
Jakob Wenzel <wenzel@rs.tu-darmstadt.de>
2025-11-08 00:32:37 +0100

    fix wrong import, fix license headers
    Signed-off-by: Jakob Wenzel <wenzel@rs.tu-darmstadt.de>

--- src/com/xilinx/rapidwright/design/xdc/parser/AddCellsToPblockCommand.java
+++ src/com/xilinx/rapidwright/design/xdc/parser/AddCellsToPblockCommand.java
@@ -1,15 +1,32 @@
+/*
+ * Copyright (c) 2025, Advanced Micro Devices, Inc.
+ * All rights reserved.
+ *
+ * Author: Jakob Wenzel, Technical University of Darmstadt
+ *
+ * This file is part of RapidWright.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.xilinx.rapidwright.design.xdc.parser;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.xilinx.rapidwright.design.xdc.PBlockConstraint;
 import com.xilinx.rapidwright.design.xdc.UnsupportedConstraintElement;
 import com.xilinx.rapidwright.design.xdc.XDCConstraints;
-import sun.security.krb5.internal.crypto.Des;
 import tcl.lang.Command;
 import tcl.lang.Interp;
 import tcl.lang.TclException;
