236e9aaaa14168f1723012f5ffe25251b659d4c7
Chris Lavin <chris.lavin@amd.com>
2025-11-17 21:21:36 -0700

    Fix selection of unexpanded tree items
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- src/com/xilinx/rapidwright/gui/NetlistTreeWidget.java
+++ src/com/xilinx/rapidwright/gui/NetlistTreeWidget.java
@@ -38,7 +38,7 @@ import com.xilinx.rapidwright.edif.EDIFHierNet;
 import com.xilinx.rapidwright.edif.EDIFHierPortInst;
 import com.xilinx.rapidwright.edif.EDIFNet;
 import com.xilinx.rapidwright.edif.EDIFNetlist;
-import com.xilinx.rapidwright.edif.EDIFPort;
+import com.xilinx.rapidwright.edif.EDIFTools;
 
 /**
  * Widget that allows a tree view of the provided netlist.
@@ -52,6 +52,13 @@ public class NetlistTreeWidget extends QTreeWidget {
     private Map<String, QTreeWidgetItem> objectLookup = new HashMap<>();
 
     private static final String DUMMY = "_*DUMMY*_";
+    private static final String LEAF_CELLS = "Leaf Cells";
+    private static final String PORTS = "Ports";
+    private static final String NETS = "Nets";
+
+    public static final String INST_ID = "INST:";
+    public static final String NET_ID = "NET:";
+    public static final String PORT_ID = "PORT:";
 
     public NetlistTreeWidget(String header, EDIFNetlist netlist) {
         this.netlist = netlist;
@@ -78,12 +85,12 @@ public class NetlistTreeWidget extends QTreeWidget {
         boolean isTop = inst.isTopLevelInst();
 
         QTreeWidgetItem ports = new QTreeWidgetItem(curr);
-        ports.setText(0, "Ports (" + cell.getPorts().size() + ")");
+        ports.setText(0, PORTS + " (" + cell.getPorts().size() + ")");
 
         for (EDIFHierPortInst portInst : inst.getHierPortInsts()) {
             QTreeWidgetItem n = new QTreeWidgetItem(ports);
             n.setData(0, 0, portInst);
-            String portLookup = "PORT:" + (isTop ? portInst.getPortInst().getName() : portInst.toString());
+            String portLookup = PORT_ID + (isTop ? portInst.getPortInst().getName() : portInst.toString());
             n.setData(1, 0, portLookup);
             n.setText(0, portInst.getPortInst().getName() + " (" + portInst.getPortInst().getDirection() + ")");
             objectLookup.put(portLookup, n);
@@ -92,14 +99,14 @@ public class NetlistTreeWidget extends QTreeWidget {
 
 
         QTreeWidgetItem nets = new QTreeWidgetItem(curr);
-        nets.setText(0, "Nets (" + cell.getNets().size() + ")");
+        nets.setText(0, NETS + " (" + cell.getNets().size() + ")");
         List<EDIFNet> edifNets = new ArrayList<>(cell.getNets());
         Collections.sort(edifNets);
         for (EDIFNet net : edifNets) {
             QTreeWidgetItem n = new QTreeWidgetItem(nets);
             EDIFHierNet hierNet = inst.getNet(net.getName());
             n.setData(0, 0, hierNet);
-            String netLookup = "NET:" + hierNet.toString();
+            String netLookup = NET_ID + hierNet.toString();
             n.setData(1, 0, netLookup);
             n.setText(0, net.getName());
             objectLookup.put(netLookup, n);
@@ -120,12 +127,12 @@ public class NetlistTreeWidget extends QTreeWidget {
         Collections.sort(nonLeaves);
 
         QTreeWidgetItem leafCells = new QTreeWidgetItem(curr);
-        leafCells.setText(0, "Leaf Cells (" + leaves.size() + ")");
+        leafCells.setText(0, LEAF_CELLS + " (" + leaves.size() + ")");
         for (EDIFHierCellInst i : leaves) {
             QTreeWidgetItem leaf = new QTreeWidgetItem(leafCells);
-            leaf.setText(0, i.getInst().getName() + " (" + i.getCellName() + ")");
             leaf.setData(0, 0, i);
-            String leafLookup = "INST:" + i.toString();
+            leaf.setText(0, i.getInst().getName() + " (" + i.getCellName() + ")");
+            String leafLookup = INST_ID + i.toString();
             leaf.setData(1, 0, leafLookup);
             objectLookup.put(leafLookup, leaf);
         }
@@ -136,7 +143,7 @@ public class NetlistTreeWidget extends QTreeWidget {
             HierCellInstTreeWidgetItem cellInst = new HierCellInstTreeWidgetItem(curr);
             cellInst.setText(0, i.getInst().getName() + " (" + i.getCellName() + ")");
             cellInst.setInst(i);
-            String instLookup = "INST:" + i.toString();
+            String instLookup = INST_ID + i.toString();
             cellInst.setData(1, 0, instLookup);
             objectLookup.put(instLookup, cellInst);
             QTreeWidgetItem dummy = new QTreeWidgetItem(cellInst);
@@ -173,6 +180,53 @@ public class NetlistTreeWidget extends QTreeWidget {
     }
 
     public QTreeWidgetItem getItemByStringLookup(String lookup) {
-        return objectLookup.get(lookup);
+        QTreeWidgetItem item = objectLookup.get(lookup);
+        if (item != null) {
+            return item;
+        }
+
+        // We need to expand the tree to create the item first
+        String hierPath = lookup.substring(lookup.indexOf(":") + 1);
+
+        String[] parts = hierPath.split(EDIFTools.EDIF_HIER_SEP);
+        QTreeWidgetItem currItem = rootItem;
+        EDIFHierCellInst currInst = netlist.getTopHierCellInst();
+
+        boolean isInst = lookup.startsWith(INST_ID);
+        for (int i = 0; i < parts.length; i++) {
+            EDIFHierCellInst nextInst = currInst.getChild(parts[i]);
+            currInst = nextInst == null ? currInst : nextInst;
+            currItem = getChildItem(i == parts.length - 1, currInst, parts[i], currItem, isInst);
+        }
+
+        return currItem;
+    }
+
+    private QTreeWidgetItem getChildItem(boolean isLastLevel, EDIFHierCellInst cellInst, String instName,
+            QTreeWidgetItem currItem, boolean isInst) {
+        // Expand this instance if it hasn't been expanded yet
+        if (currItem.childCount() == 1 && currItem.child(0).text(0).equals(DUMMY)) {
+            currItem = populateCellInst(currItem, isInst ? cellInst.getParent() : cellInst);
+        }
+        for (int j = 0; j < currItem.childCount(); j++) {
+            QTreeWidgetItem child = currItem.child(j);
+            String text = child.text(0);
+            if (isLastLevel) {
+                if (text.startsWith(LEAF_CELLS) || text.startsWith(NETS) || text.startsWith(PORTS)) {
+                    child.setExpanded(true);
+                    for (int k = 0; k < child.childCount(); k++) {
+                        QTreeWidgetItem leaf = child.child(k);
+                        String leafText = leaf.text(0);
+                        if (leafText.startsWith(instName)) {
+                            return leaf;
+                        }
+                    }
+                }
+            }
+            if (text.startsWith(instName)) {
+                return child;
+            }
+        }
+        return null;
     }
 }
