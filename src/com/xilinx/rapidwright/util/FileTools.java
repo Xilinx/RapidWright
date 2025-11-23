87536b2ada746d37cc7c9e1daa8d9acda9a69fa1
Chris Lavin <chris.lavin@amd.com>
2025-11-22 23:12:53 -0700

    [VersalClockRouting] VDISTR tree storage map for Versal (#1311)
    * [VersalClockRouting] VDISTR tree storage map for Versal
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Preliminary v80 support
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Refactoring to use preferred clk root; adds VersalClockTree object
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Update storage format and file for vdistr paths on Versal
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Increasing the search space for clock root column candidates
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Apply suggestions from code review
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Add an assert
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Adds all Versal devices (except vp1902)
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Adding test to check that Versal distribution template in use
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * New data file fixes missing entries
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    ---------
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- src/com/xilinx/rapidwright/util/FileTools.java
+++ src/com/xilinx/rapidwright/util/FileTools.java
@@ -127,6 +127,8 @@ public class FileTools {
     public static final String PART_DB_PATH = DATA_FOLDER_NAME + File.separator + "parts.db";
     /** Location of the cell pins default data file */
     public static final String CELL_PIN_DEFAULTS_FILE_NAME = DATA_FOLDER_NAME + File.separator + "cell_pin_defaults.dat";
+    /** Location of the Versal VDISTR Tree paths for clock routing */
+    public static final String VERSAL_VDISTR_TREES_FILE_NAME = DATA_FOLDER_NAME + File.separator + "versal_vdistr_trees.dat";
     /** Location of cached routethru helper files */
     public static final String ROUTETHRU_FOLDER_NAME = DATA_FOLDER_NAME + File.separator + "routeThrus";
     /** Common instance of the Kryo class for serialization purposes */
