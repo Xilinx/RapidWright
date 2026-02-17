7ab84b1123f870ec8aa158e50d849584685c222b
Chris Lavin <chris.lavin@amd.com>
2026-02-17 11:21:05 -0700

    Export schematic viewer scene as a PDF or SVG vector graphic. (#1344)
    * Export schematic as a PDF or SVG vector graphic.
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    * Fix export to match schematic size
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>
    
    ---------
    
    Signed-off-by: Chris Lavin <chris.lavin@amd.com>

--- src/com/xilinx/rapidwright/gui/UiTools.java
+++ src/com/xilinx/rapidwright/gui/UiTools.java
@@ -25,37 +25,51 @@ package com.xilinx.rapidwright.gui;
 
 import java.io.File;
 
+import com.trolltech.qt.core.QRectF;
+import com.trolltech.qt.core.QSize;
+import com.trolltech.qt.core.QSizeF;
+import com.trolltech.qt.gui.QGraphicsScene;
 import com.trolltech.qt.gui.QPainter;
 import com.trolltech.qt.gui.QPrinter;
 import com.trolltech.qt.svg.QSvgGenerator;
 
 public class UiTools {
+
     private UiTools() {
 
     }
 
-    public static void saveAsSvg(TileScene scene, File file) {
+    private static final double EXPORT_MARGIN = 10.0;
 
-        QSvgGenerator svgGen = new QSvgGenerator();
-
-        svgGen.setFileName(file.toString());
-        svgGen.setSize(scene.getSceneSize());
-        svgGen.setViewBox(scene.sceneRect());
-        svgGen.setTitle("RapidWright Scene");
-        svgGen.setDescription(scene.getClass().getSimpleName());
-
-        QPainter svgPainter = new QPainter(svgGen);
-        scene.render(svgPainter);
-        svgPainter.end();
+    private static QRectF getExportRect(QGraphicsScene scene) {
+        QRectF rect = scene.itemsBoundingRect();
+        rect.adjust(-EXPORT_MARGIN, -EXPORT_MARGIN, EXPORT_MARGIN, EXPORT_MARGIN);
+        return rect;
     }
 
-    public static void saveAsPdf(TileScene scene, File file) {
-        QPrinter printer = new QPrinter();
+    public static void saveAsPdf(QGraphicsScene scene, File file) {
+        QRectF exportRect = getExportRect(scene);
+        QPrinter printer = new QPrinter(QPrinter.PrinterMode.HighResolution);
         printer.setOutputFormat(QPrinter.OutputFormat.PdfFormat);
         printer.setOutputFileName(file.toString());
-        printer.setPageSize(QPrinter.PageSize.Letter);
+        // Set custom page size to fit the schematic content
+        printer.setPaperSize(new QSizeF(exportRect.width(), exportRect.height()), QPrinter.Unit.Point);
         QPainter pdfPainter = new QPainter(printer);
-        scene.render(pdfPainter);
+        scene.render(pdfPainter, new QRectF(), exportRect);
         pdfPainter.end();
+    }
+
+    public static void saveAsSvg(QGraphicsScene scene, File file) {
+        QRectF exportRect = getExportRect(scene);
+        QSvgGenerator svgGen = new QSvgGenerator();
+        svgGen.setFileName(file.toString());
+        svgGen.setSize(new QSize((int) exportRect.width(), (int) exportRect.height()));
+        svgGen.setViewBox(new QRectF(0, 0, exportRect.width(), exportRect.height()));
+        svgGen.setTitle("RapidWright Schematic");
+        svgGen.setDescription("Schematic Scene");
+
+        QPainter svgPainter = new QPainter(svgGen);
+        scene.render(svgPainter, new QRectF(), exportRect);
+        svgPainter.end();
     }
 }
